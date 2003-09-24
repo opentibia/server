a73a1422f592c99660fd30d94b9678e0882b62b4
Timmit <timmit@users.sourceforge.net>
2003-09-24 20:10:13 +0000

    *** empty log message ***

--- otserv/trunk/player_mem.h
+++ otserv/trunk/player_mem.h
@@ -20,6 +20,9 @@
 // $Id$
 //////////////////////////////////////////////////////////////////////
 // $Log$
+// Revision 1.7  2003/09/24 20:10:13  timmit
+// *** empty log message ***
+//
 // Revision 1.6  2003/09/08 13:28:41  tliffrag
 // Item summoning and maploading/saving now working.
 //
@@ -42,15 +45,54 @@
 #include "pos.h"
 #include <string>
 
+enum skills_t {
+    SKILL_FIST,
+    SKILL_CLUB,
+    SKILL_SWORD,
+    SKILL_AXE,
+    SKILL_DIST,
+    SKILL_SHIELD,
+    SKILL_FISH
+};
+enum skillsid_t {
+    SKILL_LEVEL,
+    SKILL_TRIES
+};
+
    class player_mem : public ::Memory {
         public:
+            void load();
             void save();
 			player_mem();
             std::string name, passwd;
 			int color_hair, color_shirt, color_legs, color_shoes;
 			int lookdir;
 			position pos;
-
+			
+			// health, health max.
+			int health, health_max;
+			// mana, mana max.
+			int mana, mana_max;
+			// food level
+			int food_level;
+			// inventory
+			// TODO: how do you want to store the inventory?
+			// use a list of Item?
+			// capacity ( based off what is in inventory ), capacity max.
+			int cap, cap_max;
+			// level
+			int level;
+            // experience
+            unsigned long experience;
+			// magic level
+			int mag_level;
+            //mana spent ( to know when the character will advance in magic level )
+			unsigned long mana_spent;
+			// skills, # of tries on each skill ( to know when the character will advance )
+			// 2 sub-elements.  [ skill ][ SKILL_LEVEL ] is the current level of that skill
+			// [ skill ][ SKILL_TRIES ] is how many tries on that skill ( to determine when the skill advances )
+			int skills[ 7 ][ 2 ];
+			
             unsigned int pnum;
 
     }; // class player_mem : public Memory
