71d065c2e6722c3057374685a22bc3a270051fba
Johannes Rosenqvist <xeroc81@gmail.com>
2004-11-28 02:33:02 +0000

    *** empty log message ***

---
+++ otserv/trunk/tools.h
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////////////////////////
+// OpenTibia - an opensource roleplaying game
+//////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//////////////////////////////////////////////////////////////////////
+
+
+#ifndef __OTSERV_TOOLS_H
+#define __OTSERV_TOOLS_H
+
+
+int random_range(int lowest_number, int highest_number);
+void hexdump(unsigned char *_data, int _len);
+char upchar(char c);
+void upper(char *upstr, char *str);
+void upper(char *upstr, char *str, int n);
+
+#endif
