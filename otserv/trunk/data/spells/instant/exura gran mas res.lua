c5cfef1d84bcecaac02201d22f13bd70a0ab0ab3
Ricochet <ricochet_03@users.sourceforge.net>
2005-01-15 23:47:50 +0000

    little bug fixed

--- otserv/trunk/data/spells/instant/exura gran mas res.lua
+++ otserv/trunk/data/spells/instant/exura gran mas res.lua
@@ -6,6 +6,7 @@
 -- perhaps we should write some docu
 
 -- the id of the creature we are attacking, following, etc.
+
 area = {
 {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
 {0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0},
@@ -19,6 +20,7 @@ area = {
 {0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0},
 {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}
 }
+
 needDirection = false
 areaEffect = NM_ME_MAGIC_ENERGIE
 animationEffect = NM_ANI_NONE
@@ -32,16 +34,23 @@ MassHealingObject = MagicDamageObject(animationEffect, damageEffect, animationCo
 
 function onCast(cid, creaturePos, level, maglv, var)
 centerpos = {x=creaturePos.x, y=creaturePos.y, z=creaturePos.z}
+
 n = tonumber(var)   -- try to convert it to a number
+
 	if n ~= nil then
 		MassHealingObject.minDmg = var+0
 		MassHealingObject.maxDmg = var+0
+
 	else
 		MassHealingObject.minDmg = ((level+maglv)*3) * 1.0
 		MassHealingObject.maxDmg = ((level+maglv)*3) * 2.2
+
 	end
-return doAreaMagic(cid, centerpos, needDirection, areaEffect, area, GreatEnergyBeamObject:ordered())
+
+return doAreaMagic(cid, centerpos, needDirection, areaEffect, area, MassHealingObject:ordered())
 end
+
+
 
 
 
