eb62312dec156db9141ded4c956ca2cb2633eb6e
mips <mips_act@users.sourceforge.net>
2005-05-27 19:25:10 +0000

    * add items on ground for some spells

--- otserv/trunk/spells.cpp
+++ otserv/trunk/spells.cpp
@@ -879,9 +879,12 @@ int SpellScript::luaActionMakeArrows(lua_State *L){
 			magicTarget.damageEffect = 12; //NM_ME_MAGIC_ENERGIE = 12
 			Item* new_item = Item::CreateItem(id,count);
 			if(!player->addItem(new_item)){
-				delete new_item;
+				spell->game->addThing(NULL,player->pos,new_item);
+				new_item->pos = player->pos;
+				spell->game->sendAddThing(NULL,player->pos,new_item);
+				/*delete new_item;
 				magicTarget.damageEffect = 2; //NM_ME_PUFF  
-  				magicTarget.manaCost = 0;
+  				magicTarget.manaCost = 0;*/
 			} 
  		}
  		
@@ -933,9 +936,13 @@ int SpellScript::luaActionMakeFood(lua_State *L){
   			
   			Item* new_item = Item::CreateItem(foodtype,count);
 			if(!player->addItem(new_item)){
-				delete new_item;
+				//add item on the ground
+				spell->game->addThing(NULL,player->pos,new_item);
+				new_item->pos = player->pos;
+				spell->game->sendAddThing(NULL,player->pos,new_item);
+				/*delete new_item;
 				magicTarget.damageEffect = 2; //NM_ME_PUFF  
-  				magicTarget.manaCost = 0;
+  				magicTarget.manaCost = 0;*/
 			} 
 		}
   
