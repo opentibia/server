cd3889ebbd23aa69087f70a05caa5a96cb18969f
mips <mips_act@users.sourceforge.net>
2005-08-02 18:02:02 +0000

    * Allow only protocol versions 7.40 and 7.41

--- otserv/trunk/otserv.cpp
+++ otserv/trunk/otserv.cpp
@@ -238,7 +238,7 @@ OTSYS_THREAD_RETURN ConnectionHandler(void *dat)
 			msg.GetU32();
 			std::string name     = msg.GetString();
 			std::string password = msg.GetString();
-			if(version < 740){
+			if(version != 740 && version != 741){
 				msg.Reset();
 				msg.AddByte(0x14);
 				msg.AddString("Only clients with protocol 7.4 allowed!");
