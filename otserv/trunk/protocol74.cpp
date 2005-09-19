8781f0796399836fff03c3ee68a6a7ee2ab6c99e
Johannes Rosenqvist <xeroc81@gmail.com>
2005-09-19 13:00:33 +0000

    *** empty log message ***

--- otserv/trunk/protocol74.cpp
+++ otserv/trunk/protocol74.cpp
@@ -1009,23 +1009,10 @@ void Protocol74::parseUpArrowContainer(NetworkMessage &msg)
 
 void Protocol74::parseThrow(NetworkMessage &msg)
 {
-/*
-  78 
-  ff ff 
-  40 00 
-  01 
-  6a 0a 
-  01 
-  ff ff 
-  40 00 
-  05 
-  0a
-*/     
-
   unsigned short from_x     = msg.GetU16();
   unsigned short from_y     = msg.GetU16(); 
   unsigned char  from_z     = msg.GetByte();
-	unsigned short itemid = msg.GetU16();
+	unsigned short itemid     = msg.GetU16();
   unsigned char  from_stack = msg.GetByte();
   unsigned short to_x       = msg.GetU16();
   unsigned short to_y       = msg.GetU16(); 
