89a274b613d9069648714f2ee66d37e5eed296fc
Johannes Rosenqvist <xeroc81@gmail.com>
2006-01-15 01:01:42 +0000

    * changed Player::__internalAddThing failure error to a warning

--- otserv/branches/revmovesys/player.cpp
+++ otserv/branches/revmovesys/player.cpp
@@ -2444,8 +2444,8 @@ void Player::__internalAddThing(uint32_t index, Thing* thing)
 	if(index > 0 && index < 11){
 		if(items[index]){
 #ifdef __DEBUG__MOVESYS__
-			std::cout << "Failure: [Player::__internalAddThing], player: " << getName() << ", items[index] is not empty" << std::endl;
-			int *a = NULL; *a = 1;
+			std::cout << "Warning: [Player::__internalAddThing], player: " << getName() << ", items[index] is not empty." << std::endl;
+			//int *a = NULL; *a = 1;
 #endif
 			return;
 		}
