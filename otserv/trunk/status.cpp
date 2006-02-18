3a46639e7c4ba45ceb317d8661585494dcbd7f16
Johannes Rosenqvist <xeroc81@gmail.com>
2006-02-18 19:40:05 +0000

    * added client version info

--- otserv/trunk/status.cpp
+++ otserv/trunk/status.cpp
@@ -80,7 +80,7 @@ std::string Status::getStatusString(){
 	xmlSetProp(p, (const xmlChar*) "url", (const xmlChar*)g_config.getGlobalString("url", "").c_str());
 	xmlSetProp(p, (const xmlChar*) "server", (const xmlChar*)"otserv");
 	xmlSetProp(p, (const xmlChar*) "version", (const xmlChar*)"0.5.0_CVS");
-	//xmlSetProp(p, (const xmlChar*) "version", (const xmlChar*)"0.4.1");
+	xmlSetProp(p, (const xmlChar*) "client", (const xmlChar*)"7.6");
 	xmlAddChild(root, p);
 
 	p=xmlNewNode(NULL,(const xmlChar*)"owner");
