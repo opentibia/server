a81ef2c6401173c18018350d34ae2584901a1279
mips <mips_act@users.sourceforge.net>
2006-02-22 21:36:46 +0000

    * fix overflow

--- otserv/trunk/player.h
+++ otserv/trunk/player.h
@@ -454,7 +454,7 @@ protected:
 	LightInfo itemsLight;
 	
 	#ifdef __SKULLSYSTEM__
-	long redSkullTicks;
+	uint64_t redSkullTicks;
 	skulls_t skull;
 	typedef std::set<long> AttackedSet;
 	AttackedSet attackedSet;
