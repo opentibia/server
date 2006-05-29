99f8ec3e7d2545735c75376a12585b4a4cec9887
Johannes Rosenqvist <xeroc81@gmail.com>
2006-05-29 14:03:37 +0000

    *** empty log message ***

---
+++ otserv/trunk/data/actions/scripts/witch1.lua
@@ -0,0 +1,15 @@
+function onUse(cid, item, frompos, item2, topos)
+if item.uid == 1008 then
+    queststatus = getPlayerStorageValue(cid,1008)
+    if queststatus == -1 or queststatus == 0 then
+      doPlayerSendTextMessage(cid,22,"You have found some diamonds.")
+      doPlayerAddItem(cid,2145,3)
+      setPlayerStorageValue(cid,1008,1)
+    else
+      doPlayerSendTextMessage(cid,22,"The chest is empty.")
+    end
+else
+return 0
+end
+return 1
+end
---
+++ otserv/trunk/data/actions/scripts/witch2.lua
@@ -0,0 +1,15 @@
+function onUse(cid, item, frompos, item2, topos)
+if item.uid == 1009 then
+    queststatus = getPlayerStorageValue(cid,1009)
+    if queststatus == -1 or queststatus == 0 then
+      doPlayerSendTextMessage(cid,22,"You have found an amazon shield.")
+      doPlayerAddItem(cid,2537,1)
+      setPlayerStorageValue(cid,1009,1)
+    else
+      doPlayerSendTextMessage(cid,22,"The chest is empty.")
+    end
+else
+return 0
+end
+return 1
+end
