ec04d1b557bee61e66439cce67f06b309cef613a
mips <mips_act@users.sourceforge.net>
2006-06-11 19:01:48 +0000

    * New lua script interface preview

--- otserv/branches/revbattlesys/luascript.cpp
+++ otserv/branches/revbattlesys/luascript.cpp
@@ -23,6 +23,8 @@
 #include <iostream>
 
 #include "luascript.h"
+#include "player.h"
+#include "item.h"
 
 LuaScript::LuaScript()
 {
@@ -41,7 +43,7 @@ int LuaScript::openFile(const char *filename)
 {
 	luaState = lua_open();
 
-	if (lua_dofile(luaState, filename))
+	if(luaL_dofile(luaState, filename))
 		return false;
 
   return true;
@@ -134,4 +136,241 @@ void LuaScript::setField (lua_State *L, const char *index, int val)
 	lua_pushstring(L, index);
 	lua_pushnumber(L, (double)val);
 	lua_settable(L, -3);
+}
+
+
+ScriptEnviroment::ScriptEnviroment()
+{
+	resetEnv();
+	m_lastUID = 70000;
+}
+
+ScriptEnviroment::~ScriptEnviroment()
+{
+	//
+}
+
+void ScriptEnviroment::resetEnv()
+{
+	m_interface = NULL;
+	m_scriptId = 0;
+}
+	
+void ScriptEnviroment::setScriptId(long scriptId, LuaScriptInterface* scriptInterface)
+{
+	m_scriptId = scriptId;
+	m_interface = scriptInterface;
+}
+
+void ScriptEnviroment::setEventDesc(const std::string& desc)
+{
+	m_eventdesc = desc;
+}
+	
+void ScriptEnviroment::getEventInfo(long& scriptId, std::string& desc, LuaScriptInterface* scriptInterface)
+{
+	scriptId = m_scriptId;
+	desc = m_eventdesc;
+	scriptInterface = m_interface;
+}
+
+void ScriptEnviroment::addUniqueThing(Thing* thing)
+{
+	Item* item = thing->getItem();
+	if(item && item->getUniqueId() != 0 ){
+		unsigned short uid = item->getUniqueId();
+
+		Thing* tmp = m_globalMap[uid];
+		if(!tmp){
+			m_globalMap[uid] = thing;
+		}
+		else{
+			std::cout << "Duplicate uniqueId " <<  uid << std::endl;
+		}
+	}
+}
+
+long ScriptEnviroment::addThing(Thing* thing)
+{
+	//TODO
+	return 0;
+}
+	
+Thing* ScriptEnviroment::getThingByUID(long uid)
+{
+	//TODO
+	return NULL;
+}
+
+Item* ScriptEnviroment::getItemByUID(long uid)
+{
+	//TODO
+	return NULL;
+}
+
+Creature* ScriptEnviroment::getCreatureByUID(long uid)
+{
+	//TODO
+	return NULL;
+}
+
+Player* ScriptEnviroment::getPlayerByUID(long uid)
+{
+	//TODO
+	return NULL;
+}
+
+
+LuaScriptInterface::LuaScriptInterface()
+{
+	m_luaState = NULL;
+	if(!initState()){
+		//handle error
+	}
+}
+
+LuaScriptInterface::~LuaScriptInterface()
+{
+	closeState();
+}
+	
+bool LuaScriptInterface::reInitState()
+{
+	closeState();
+	return initState();
+}
+
+long LuaScriptInterface::getLastLuaError()
+{
+	return m_lastLuaError;
+}
+
+void LuaScriptInterface::dumpLuaStack()
+{
+	int a = lua_gettop(m_luaState);
+	std::cout <<  "stack side: " << a << std::endl;
+	for(int i = 1; i <= a ; i++){
+		std::cout << lua_typename(m_luaState, lua_type(m_luaState,-i)) << " " << lua_topointer(m_luaState, -i) << std::endl;
+	}
+}
+
+long LuaScriptInterface::loadFile(const std::string& file, const std::string& eventName)
+{
+	int ret = luaL_loadfile(m_luaState, file.c_str());
+	if(ret != 0){
+		 m_lastLuaError = ret;
+		return -1;
+	}
+	if(lua_isfunction(m_luaState, -1) == 0){
+		return -1;
+	}
+	//
+	ret = lua_pcall(m_luaState, 0, 0, 0);
+	if(ret != 0){
+		return -1;
+	}
+	//
+	lua_getfield(m_luaState, LUA_REGISTRYINDEX, "EVENTS");
+	if(lua_istable(m_luaState, -1) == 0){
+		return -1;
+	}
+	//
+	lua_getglobal(m_luaState, eventName.c_str());
+	if(lua_isfunction(m_luaState, -1) == 0){
+		return -1;
+	}
+	
+	lua_pushnumber(m_luaState, m_runningEventId++);
+	lua_pushvalue(m_luaState, -2);
+	lua_rawset(m_luaState, -4);
+	lua_pop(m_luaState, 2);
+
+	return 1;
+}
+
+const std::string& LuaScriptInterface::getFileById(long scriptId)
+{
+	static std::string unk = "(Unknow script file)";
+	ScriptsCache::iterator it = m_cacheFiles.find(scriptId);
+	if(it != m_cacheFiles.end()){
+		return it->second;
+	}
+	else{
+		return unk;
+	}
+}
+	
+ScriptEnviroment* LuaScriptInterface::getScriptEnv()
+{
+	return &m_scriptEnv;
+}
+
+void LuaScriptInterface::reportError(const std::string& error_desc)
+{
+	ScriptEnviroment* env = getScriptEnv();
+	long fileId;
+	std::string event_desc;
+	LuaScriptInterface* scriptInterface;
+	env->getEventInfo(fileId, event_desc, scriptInterface);
+	
+	std::cout << "Lua Script Error: ";
+	if(scriptInterface){
+		std::cout << "[" << scriptInterface->getInterfaceName() << "] " <<
+			scriptInterface->getFileById(fileId) << std::endl;
+	}
+	std::cout << event_desc << std::endl;
+	std::cout << error_desc << std::endl;
+}
+
+
+std::string LuaScriptInterface::getInterfaceName()
+{
+	return "Base Lua Interface";
+}
+
+bool LuaScriptInterface::pushFunction(long functionId)
+{
+	lua_getfield(m_luaState, LUA_REGISTRYINDEX, "EVENTS");
+	if(lua_istable(m_luaState, -1) == 0){
+		return false;
+	}
+	lua_pushnumber(m_luaState, functionId);
+	lua_rawget(m_luaState, -2);
+	lua_remove(m_luaState, -2);
+	if(lua_isfunction(m_luaState, -1) == 0){
+		return false;
+	}
+	return true;
+}
+
+bool LuaScriptInterface::initState()
+{
+	m_luaState = luaL_newstate();
+	if(!m_luaState){
+		return false;
+	}
+	luaopen_base(m_luaState);
+	luaopen_math(m_luaState);
+	luaopen_string(m_luaState);
+	
+	registerFunctions();
+	
+	lua_newtable(m_luaState);
+	lua_setfield(m_luaState, LUA_REGISTRYINDEX, "EVENTS");
+	
+	m_runningEventId = 1000;
+	return true;
+}
+
+bool LuaScriptInterface::closeState()
+{
+	m_cacheFiles.clear();
+	lua_close(m_luaState);
+	return true;
+}
+	
+void LuaScriptInterface::registerFunctions()
+{
+	//TODO
+	//lua_register(L, "name", C_function);
 }
--- otserv/branches/revbattlesys/luascript.h
+++ otserv/branches/revbattlesys/luascript.h
@@ -23,6 +23,8 @@
 #define __LUASCRIPT_H__
 
 #include <string>
+#include <map>
+
 extern "C"
 {
 #include <lua.h>
@@ -30,8 +32,10 @@ extern "C"
 #include <lualib.h>
 }
 
-extern "C" struct lua_State;
-
+class Thing;
+class Creature;
+class Player;
+class Item;
 
 class LuaScript
 {
@@ -58,5 +62,91 @@ protected:
 	lua_State*  luaState;  // our lua state
 };
 
+class LuaScriptInterface;
+
+class ScriptEnviroment
+{
+public:
+	ScriptEnviroment();
+	~ScriptEnviroment();
+	
+	void resetEnv();
+	
+	void setScriptId(long scriptId, LuaScriptInterface* scriptInterface);
+	void setEventDesc(const std::string& desc);
+	
+	void getEventInfo(long& scriptId, std::string& desc, LuaScriptInterface* scriptInterface);
+	
+	static void addUniqueThing(Thing* thing);
+	long addThing(Thing* thing);
+	
+	Thing* getThingByUID(long uid);
+	Item* getItemByUID(long uid);
+	Creature* getCreatureByUID(long uid);
+	Player* getPlayerByUID(long uid);
+	
+private:
+	typedef std::map<long, Thing*> ThingMap;
+	//typedef std::map<long, Matrix*> AreaMap;
+	//script file id
+	long m_scriptId;
+	LuaScriptInterface* m_interface;
+	//script event desc
+	std::string m_eventdesc;
+	
+	//unique id map
+	static ThingMap m_globalMap;
+	//item/creature map
+	ThingMap m_localMap;
+	long m_lastUID;
+	//area map
+	//static AreaMap areaMap;
+};
+
+class LuaScriptInterface
+{
+public:
+	LuaScriptInterface();
+	virtual ~LuaScriptInterface();
+	
+	bool reInitState();
+	
+	long loadFile(const std::string& file, const std::string& eventName);
+	const std::string& getFileById(long scriptId);
+	
+	static ScriptEnviroment* getScriptEnv();
+	
+	static void reportError(const std::string& error_desc);
+	
+	virtual std::string getInterfaceName();
+	long getLastLuaError();
+	void dumpLuaStack();
+	
+	
+	bool pushFunction(long functionId);
+	//push/pop common structures
+	
+protected:
+	bool initState();
+	bool closeState();
+	
+	void registerFunctions();
+	
+	//lua functions
+	
+	//
+	
+	lua_State*  m_luaState;
+	long m_lastLuaError;
+private:
+	
+	static ScriptEnviroment m_scriptEnv;
+	
+	long m_runningEventId;
+	
+	//script file cache
+	typedef std::map<long , std::string> ScriptsCache;
+	ScriptsCache m_cacheFiles;
+};
 
 #endif  // #ifndef __LUASCRIPT_H__
