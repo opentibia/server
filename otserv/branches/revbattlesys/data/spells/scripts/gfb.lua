8e007cfbda54c0fd906038961450c606179324a6
Johannes Rosenqvist <xeroc81@gmail.com>
2006-08-26 18:54:47 +0000

    *** empty log message ***

--- otserv/branches/revbattlesys/data/spells/scripts/gfb.lua
+++ otserv/branches/revbattlesys/data/spells/scripts/gfb.lua
@@ -1,14 +1,15 @@
 local combat = createCombatHealthObject()
 local area = createCombatArea( { {1, 1, 1}, {1, 1, 1}, {1, 1, 1} } )
 
+setCombatArea(combat, area)
 setCombatParam(combat, CONST_COMBAT_HEALTHTYPE, CONST_COMBAT_DAMAGE_PHYSICAL)
 setCombatParam(combat, CONST_COMBAT_EFFECTTYPE, CONST_ME_MORTAREA)
 
 function onGetPlayerMinMaxValues(cid, level, maglevel)
-min = -(level * 2 + maglevel * 3) * 1.3 - 30
-max = -(level * 2 + maglevel * 3) * 1.7
+	min = -(level * 2 + maglevel * 3) * 1.3 - 30
+	max = -(level * 2 + maglevel * 3) * 1.7
 
-return min, max
+	return min, max
 end
 
 setCombatCallback(combat, CONST_COMBAT_MINMAXCALLBACK, "onGetPlayerMinMaxValues")
@@ -17,4 +18,3 @@ function onUseRune(cid, pos, var)
 	-- gfb
 	doAreaCombat(cid, combat, area, pos)
 end
-
--- otserv/branches/revbattlesys/data/spells/scripts/sd.lua
+++ otserv/branches/revbattlesys/data/spells/scripts/sd.lua
@@ -2,18 +2,20 @@ local combat = createCombatHealthObject()
 setCombatParam(combat, CONST_COMBAT_HEALTHTYPE, CONST_COMBAT_DAMAGE_SUDDENDEATH)
 setCombatParam(combat, CONST_COMBAT_EFFECTTYPE, NM_ME_MORT_AREA)
 
-function onPlayerGetValues(cid, level, maglevel, values)
-	values.minDamage = (level * 2 + maglevel * 3) * 1.3 - 30
-	values.maxDamage = (level * 2 + maglevel * 3) * 1.7
+function onGetPlayerMinMaxValues(cid, level, maglevel, values)
+	min = -(level * 2 + maglevel * 3) * 1.3 - 30
+	max = -(level * 2 + maglevel * 3) * 1.7
+
+	return min, max
 end
 
---function onUse(cid, item, frompos, item2, topos)
-function onUseRune(cid, pos, var)
-	-- sd rune
-	--doAreaCombat(cid, combat, area, pos)
-	doTargetCombat(cid, combat, var)
+setCombatCallback(combat, CONST_COMBAT_MINMAXCALLBACK, "onGetPlayerMinMaxValues")
 
-	-- healing rune
-	--doTargetCombat(cid, combat, var)
-	--doTargetCombatHealth(cid, combat, var, combat.minDamage, combat.maxDamage)
+function onUseRune(cid, pos, var)
+	if var ~= nil then
+		-- sd rune
+		doAreaCombat(cid, combat, area, pos)
+	else
+		doTargetCombat(cid, combat, var)
+	end
 end
