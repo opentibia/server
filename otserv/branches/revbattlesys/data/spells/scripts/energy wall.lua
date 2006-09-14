53cf945f37b002b908c8d51a7f4e37f903364ccd
Johannes Rosenqvist <xeroc81@gmail.com>
2006-09-14 23:57:04 +0000

    *** empty log message ***

---
+++ otserv/branches/revbattlesys/data/spells/scripts/energy wall.lua
@@ -0,0 +1,35 @@
+local combat = createCombatObject(COMBAT_TYPE_HITPOINTS)
+
+local arr = {
+{1, 1, 3, 1, 1}
+}
+
+local arrDiag = {
+{0, 0, 0, 0, 0, 0, 1},
+{0, 0, 0, 0, 0, 1, 1},
+{0, 0, 0, 0, 1, 1, 0},
+{0, 0, 1, 3, 1, 0, 0},
+{0, 1, 1, 0, 0, 0, 0},
+{1, 1, 0, 0, 0, 0, 0},
+{1, 0, 0, 0, 0, 0, 0},
+}
+
+local area = createCombatArea(arr, arrDiag)
+
+setCombatArea(combat, area)
+setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_DAMAGE_ENERGY)
+setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_ENERGYHIT)
+setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
+
+function onGetPlayerMinMaxValues(cid, level, maglevel)
+	min = -(level * 2 + maglevel * 3) * 1.3 - 30
+	max = -(level * 2 + maglevel * 3) * 1.7
+
+	return min, max
+end
+
+setCombatCallback(combat, COMBAT_PARAM_MINMAXCALLBACK, "onGetPlayerMinMaxValues")
+
+function onCastSpell(cid, var)
+	doCombat(cid, combat, var)
+end
---
+++ otserv/branches/revbattlesys/data/spells/scripts/energy wave.lua
@@ -0,0 +1,37 @@
+local combat = createCombatObject(COMBAT_TYPE_HITPOINTS)
+
+local arr = {
+{1, 1, 1},
+{1, 1, 1},
+{1, 1, 1},
+{0, 1, 0},
+{0, 3, 0},
+}
+
+local arrDiag = {
+{1, 1, 1, 0, 0},
+{1, 1, 0, 0, 0},
+{1, 0, 1, 0, 0},
+{0, 0, 0, 1, 0},
+{0, 0, 0, 0, 3},
+}
+
+local area = createCombatArea(arr, arrDiag)
+
+setCombatArea(combat, area)
+setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_DAMAGE_ENERGY)
+setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_ENERGYAREA)
+setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
+
+function onGetPlayerMinMaxValues(cid, level, maglevel)
+	min = -(level * 2 + maglevel * 3) * 1.3 - 30
+	max = -(level * 2 + maglevel * 3) * 1.7
+
+	return min, max
+end
+
+setCombatCallback(combat, COMBAT_PARAM_MINMAXCALLBACK, "onGetPlayerMinMaxValues")
+
+function onCastSpell(cid, var)
+	doCombat(cid, combat, var)
+end
