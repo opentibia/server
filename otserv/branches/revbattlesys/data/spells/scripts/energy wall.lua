14d81a436fe921d0b784813328cc11c55deca1a4
Johannes Rosenqvist <xeroc81@gmail.com>
2006-09-16 18:18:02 +0000

    *** empty log message ***

--- otserv/branches/revbattlesys/data/spells/scripts/energy wall.lua
+++ otserv/branches/revbattlesys/data/spells/scripts/energy wall.lua
@@ -20,6 +20,7 @@ setCombatArea(combat, area)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_DAMAGE_ENERGY)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_ENERGYHIT)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ENERGY)
+setCombatParam(combat, COMBAT_PARAM_CREATEITEM, 1491)
 
 function onGetPlayerMinMaxValues(cid, level, maglevel)
 	min = -(level * 2 + maglevel * 3) * 1.3 - 30
