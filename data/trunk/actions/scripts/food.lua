d24b39513f1fca4e43e325a21539b1b0edfd05e3
Heliton <heliton@users.sourceforge.net>
2006-10-04 02:02:39 +0000

    * Actions.lua bug fix * Update foods * Update teleport items

--- data/trunk/actions/scripts/food.lua
+++ data/trunk/actions/scripts/food.lua
@@ -1,97 +1,93 @@
-MAX_FOOD = 1200
+MAX_REGENERATION = 1200
 function onUse(cid, item, frompos, item2, topos)
-	if item.itemid == 2328 then
-		food = 96
-	elseif item.itemid == 2362 then
-		food = 96
-	elseif item.itemid == 2363 then
-		food = 72
-	elseif item.itemid == 2666 then
-		food = 180
+	if item.itemid == 2666 then
+		regeneration = 180
 	elseif item.itemid == 2667 then
-		food = 144
+		regeneration = 144
 	elseif item.itemid == 2668 then
-		food = 120
+		regeneration = 120
 	elseif item.itemid == 2669 then
-		food = 254
+		regeneration = 0 -- big fish
 	elseif item.itemid == 2670 then
-		food = 196
+		regeneration = 0 -- shrimp
 	elseif item.itemid == 2671 then
-		food = 360
+		regeneration = 360
 	elseif item.itemid == 2672 then
-		food = 720
+		regeneration = 720
 	elseif item.itemid == 2673 then
-		food = 56
+		regeneration = 60
 	elseif item.itemid == 2674 then
-		food = 72
+		regeneration = 72
 	elseif item.itemid == 2675 then
-		food = 156
+		regeneration = 156
 	elseif item.itemid == 2676 then
-		food = 96
+		regeneration = 96
 	elseif item.itemid == 2677 then
-		food = 12
+		regeneration = 12
 	elseif item.itemid == 2678 then
-		food = 160
+		regeneration = 216
 	elseif item.itemid == 2679 then
-		food = 12
+		regeneration = 12
 	elseif item.itemid == 2680 then
-		food = 24
+		regeneration = 24
 	elseif item.itemid == 2681 then
-		food = 108
+		regeneration = 108
 	elseif item.itemid == 2682 then
-		food = 240
+		regeneration = 240
 	elseif item.itemid == 2683 then
-		food = 240
+		regeneration = 204
 	elseif item.itemid == 2684 then
-		food = 96
+		regeneration = 60
 	elseif item.itemid == 2685 then
-		food = 46
+		regeneration = 72
 	elseif item.itemid == 2686 then
-		food = 108
+		regeneration = 108
 	elseif item.itemid == 2687 then
-		food = 24
+		regeneration = 24
 	elseif item.itemid == 2688 then
-		food = 126
+		regeneration = 24
 	elseif item.itemid == 2689 then
-		food = 120
+		regeneration = 120
 	elseif item.itemid == 2690 then
-		food = 72
+		regeneration = 72
 	elseif item.itemid == 2691 then
-		food = 96
+		regeneration = 96
 	elseif item.itemid == 2695 then
-		food = 96
+		regeneration = 72
 	elseif item.itemid == 2696 then
-		food = 108
+		regeneration = 108
 	elseif item.itemid == 2787 then
-		food = 72
+		regeneration = 108
 	elseif item.itemid == 2788 then
-		food = 48
+		regeneration = 48
 	elseif item.itemid == 2789 then
-		food = 264
+		regeneration = 264
 	elseif item.itemid == 2790 then
-		food = 84
+		regeneration = 0 -- orange mushroom
 	elseif item.itemid == 2791 then
-		food = 84
+		regeneration = 108
 	elseif item.itemid == 2792 then
-		food = 140
+		regeneration = 72
 	elseif item.itemid == 2793 then
-		food = 126
+		regeneration = 0 -- mushroom
 	elseif item.itemid == 2794 then
-		food = 111
+		regeneration = 0 -- mushroom
 	elseif item.itemid == 2795 then
-		food = 136
+		regeneration = 432
 	elseif item.itemid == 2796 then
-		food = 60
+		regeneration = 60
 	elseif item.itemid == 5097 then
-		food = 48
+		regeneration = 48
+	elseif item.itemid == 6125 then
+		regeneration = 97
 	else
 		return 0
 	end
-	if getPlayerFood(cid) +	food > MAX_FOOD then
+	if getPlayerFood(cid) +	regeneration > MAX_REGENERATION then
 		doPlayerSendCancel(cid, "You are full.")
 		return 1
 	end
-	doPlayerFeed(cid, food)
+	doPlayerFeed(cid, regeneration)
 	doRemoveItem(item.uid, 1)
 	return 1
 end
--- data/trunk/actions/scripts/teleport.lua
+++ data/trunk/actions/scripts/teleport.lua
@@ -1,12 +1,12 @@
 function onUse(cid, item, frompos, item2, topos)
-	npos = {x=frompos.x,  y=frompos.y,  z=frompos.z}
+	newpos = {x = frompos.x, y = frompos.y, z = frompos.z}
 	if item.itemid == 1386 or item.itemid == 3678 then
-		npos.y = npos.y + 1
-		npos.z = npos.z - 1
-		doTeleportThing(cid, npos)
+		newpos.y = newpos.y + 1
+		newpos.z = newpos.z - 1
+		doTeleportThing(cid, newpos)
 	else
-		npos.z = npos.z + 1
-		doTeleportThing(cid, npos)
+		newpos.z = newpos.z + 1
+		doTeleportThing(cid, newpos)
 	end
 	return 1
 end
