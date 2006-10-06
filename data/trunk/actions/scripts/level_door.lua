212ff16f67a21c651d93ef445da1225157d254f5
Heliton <heliton@users.sourceforge.net>
2006-10-06 14:20:41 +0000

    Cleanup and bug fix

--- data/trunk/actions/scripts/level_door.lua
+++ data/trunk/actions/scripts/level_door.lua
@@ -1,10 +1,10 @@
 function onUse(cid, item, frompos, item2, topos) 
-    playerpos = getPlayerPosition(cid)
-    doorpos = {x = frompos.x, y = frompos.y, z = frompos.z, stackpos = 253}
 	if isInArray(CLOSED_LEVEL_DOOR, item.itemid) == 1 then
 		if TEST_SERVER == "off" then
 			if getPlayerLevel(cid) >= item.actionid - 100 then
 				doTransformItem(item.uid, item.itemid + 1)
+				playerpos = getPlayerPosition(cid)
+				doorpos = {x = frompos.x, y = frompos.y, z = frompos.z, stackpos = 253}
 				if playerpos.y == doorpos.y + 1 and playerpos.x == doorpos.x then
 					doMoveCreature(cid, 0)
 				elseif playerpos.x == doorpos.x - 1 and playerpos.y == doorpos.y then
@@ -35,12 +35,9 @@ function onUse(cid, item, frompos, item2, topos)
 	elseif isInArray(OPENED_LEVEL_DOOR, item.itemid) == 1 then
 			if isInArray(VERTICAL_OPENED_LEVEL_DOOR, item.itemid) == 1 then
 					doMoveCreature(cid, 1)
-					return 1
-			elseif isInArray(HORIZONTAL_OPENED_LEVEL_DOOR, item.itemid) == 1 then
+			else
 					doMoveCreature(cid, 2)
-					return 1
 			end			
-			doTransformItem(item.uid, item.itemid - 1)
 	else
 		return 0
     end
