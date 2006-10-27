1de9b942c50617271f516ed2442e8591e13dc250
Johannes Rosenqvist <xeroc81@gmail.com>
2006-10-27 11:40:59 +0000

    *** empty log message ***

--- otserv/branches/revbattlesys/data/spells/scripts/uh.lua
+++ otserv/branches/revbattlesys/data/spells/scripts/uh.lua
@@ -1,6 +1,8 @@
 local combat = createCombatObject(COMBAT_TYPE_HITPOINTS)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 setCombatParam(combat, COMBAT_PARAM_TARGETCASTERORTOPMOST, 1)
+setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
+setCombatParam(combat, COMBAT_PARAM_DISPEL, CONDITION_POISON)
 setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, 1.3, -30, 1.7, 0)
 
 function onCastSpell(cid, var)
