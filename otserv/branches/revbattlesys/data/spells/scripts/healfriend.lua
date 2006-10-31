d480ceefa67e206e73ec1e7afb5b1b88cf83f907
Johannes Rosenqvist <xeroc81@gmail.com>
2006-10-31 20:59:56 +0000

    *** empty log message ***

--- otserv/branches/revbattlesys/data/spells/scripts/healfriend.lua
+++ otserv/branches/revbattlesys/data/spells/scripts/healfriend.lua
@@ -1,5 +1,6 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_NONE)
+setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_NONE)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 setCombatParam(combat, COMBATPARAM_AGGRESSIVE, 0)
 setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, 0.6, -30, 1.2, 0)
