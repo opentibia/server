dbd23a43746e109d4a79807a6b260b6cbca2b0cd
Johannes Rosenqvist <xeroc81@gmail.com>
2006-11-15 21:42:05 +0000

    *** empty log message ***

--- otserv/trunk/data/spells/scripts/uh.lua
+++ otserv/trunk/data/spells/scripts/uh.lua
@@ -4,7 +4,19 @@ setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 setCombatParam(combat, COMBAT_PARAM_TARGETCASTERORTOPMOST, 1)
 setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
 setCombatParam(combat, COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
-setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, 1.3, -30, 1.7, 0)
+
+function onGetFormulaValues(cid, level, maglevel)
+	min = (level * 2 + maglevel * 3) * 1.3 - 30
+	max = (level * 2 + maglevel * 3) * 1.7
+	
+	if min < 250 then
+		min = 250
+	end
+
+	return min, max
+end
+
+setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
