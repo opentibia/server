32859a205796f8eb2a0660a70354e85a56adf423
Johannes Rosenqvist <xeroc81@gmail.com>
2006-11-29 10:59:02 +0000

    *** empty log message ***

--- otserv/trunk/data/spells/scripts/healfriend.lua
+++ otserv/trunk/data/spells/scripts/healfriend.lua
@@ -1,7 +1,7 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_HEALING)
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
-setCombatParam(combat, COMBATPARAM_AGGRESSIVE, 0)
+setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
 setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, 0.6, -30, 1.2, 0)
 
 function onCastSpell(cid, var)
