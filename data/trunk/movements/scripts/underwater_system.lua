f4756818c1b570e36978fb63650bf5d5387cb5f3
Heliton <heliton@users.sourceforge.net>
2007-02-06 16:55:47 +0000

    * remove underwater system, it will be recodded later

--- data/trunk/movements/scripts/underwater_system.lua
+++
@@ -1,9 +0,0 @@
-local drowing = createConditionObject(CONDITION_DROWN)
-addDamageCondition(drowing, 6, 2000, -20)
-
-function onStepIn(cid, item, pos)	
-	if getPlayerSlotItem(cid, CONST_SLOT_HEAD).itemid ~= 5461 then
-		doTargetCombatCondition(0, cid, drowing, CONST_ME_NONE)
-	end
-	return 1
-end
