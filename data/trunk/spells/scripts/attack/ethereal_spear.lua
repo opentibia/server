af5afc4d7a3596630bb93ea7326ff0e6ce7b04b0
pedro_b <piterb@users.sourceforge.net>
2007-09-24 01:10:04 +0000

    * Few fixes

--- data/trunk/spells/scripts/attack/ethereal_spear.lua
+++ data/trunk/spells/scripts/attack/ethereal_spear.lua
@@ -4,13 +4,10 @@ setCombatParam(combat, COMBAT_PARAM_BLOCKARMOR, TRUE)
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_ETHEREALSPEAR)
 
 function getSpellDamage(cid, weaponSkill, weaponAttack, attackStrength)
-	local attack = 25 --Spear
-	if(weaponSkill == CONST_SKILL_DISTANCE and weaponAttack > 25) then
-		attack = weaponAttack --Or the distance weapon's attack
-	end
-	local skill = CONST_SKILL_DISTANCE
+	local attack = 25 --Spear's attack
+	local skill = getPlayerSkill(cid, CONST_SKILL_DISTANCE)
 
-	local maxWeaponDamage = ((skill * attack)/20 + attack) --Max damage based on the weapon(or on a spear)
+	local maxWeaponDamage = ((skill * attack)/20 + attack)
 	local damage = -(math.random(0, maxWeaponDamage) * attackStrength) / 100) * 0.8 --0.8 is the multiplier
 
 	return damage, damage --The random part of the formula has already been made, just return the normal damage
