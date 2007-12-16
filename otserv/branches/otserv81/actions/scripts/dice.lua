f6d4cfcda24969417561482f3dbd26acf8b8f6cd
pedro_b <piterb@users.sourceforge.net>
2007-12-16 14:15:06 +0000

    * Deleted otserv81 folder since it was a copy of the data folder, not the otserv. Another one will be uploaded in a minute

--- otserv/branches/otserv81/actions/scripts/dice.lua
+++
@@ -1,8 +0,0 @@
-function onUse(cid, item, frompos, item2, topos)
-
-	local n = math.random(1,6)
-	doPlayerSay(cid, getPlayerName(cid) ..' rolled a '..n, TALKTYPE_ORANGE_1)
-	doTransformItem(item.uid, 5791 + n)
-	doSendMagicEffect(frompos, CONST_ME_CRAPS)
-	return TRUE
-end
--- otserv/branches/otserv81/actions/scripts/watch_clock.lua
+++
@@ -1,4 +0,0 @@
-function onUse(cid, item, frompos, item2, topos)
-    doPlayerSendTextMessage(cid, TEXTCOLOR_LIGHTGREEN, "The time is " .. getWorldTime() .. ".")
-    return TRUE
-end
