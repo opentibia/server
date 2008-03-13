f2399e1a1c50a00e77832d6ad0e36b564375d45b
pedro_b <piterb@users.sourceforge.net>
2008-03-13 19:24:49 +0000

    * Fixed idRange of the NPCs, now they are threated like on real Tibia. Thanks to NeWsOfTzzz for the report.

--- otserv/trunk/npc.h
+++ otserv/trunk/npc.h
@@ -1,13 +1,13 @@
 //////////////////////////////////////////////////////////////////////
 // OpenTibia - an opensource roleplaying game
 //////////////////////////////////////////////////////////////////////
-// 
+//
 //////////////////////////////////////////////////////////////////////
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
-// 
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -38,9 +38,9 @@ public:
 	bool loadNpcLib(std::string file);
 
 protected:
-	
+
 	virtual void registerFunctions();
-	
+
 	static int luaActionSay(lua_State *L);
 	static int luaActionMove(lua_State *L);
 	static int luaActionMoveTo(lua_State *L);
@@ -55,11 +55,11 @@ protected:
 	static int luaGetNpcPos(lua_State *L);
 	static int luaGetNpcName(lua_State *L);
 	static int luaGetNpcParameter(lua_State *L);
-	
+
 private:
 	virtual bool initState();
 	virtual bool closeState();
-	
+
 	bool m_libLoaded;
 };
 
@@ -68,15 +68,15 @@ class NpcEventsHandler
 public:
 	NpcEventsHandler(Npc* npc);
 	virtual ~NpcEventsHandler();
-	
+
 	virtual void onCreatureAppear(const Creature* creature){};
 	virtual void onCreatureDisappear(const Creature* creature){};
 	virtual void onCreatureMove(const Creature* creature, const Position& oldPos, const Position& newPos){};
 	virtual void onCreatureSay(const Creature* creature, SpeakClasses, const std::string& text){};
 	virtual void onThink(){};
-	
+
 	bool isLoaded();
-	
+
 protected:
 	Npc* m_npc;
 	bool m_loaded;
@@ -87,16 +87,16 @@ class NpcScript : public NpcEventsHandler
 public:
 	NpcScript(std::string file, Npc* npc);
 	virtual ~NpcScript();
-	
+
 	virtual void onCreatureAppear(const Creature* creature);
 	virtual void onCreatureDisappear(const Creature* creature);
 	virtual void onCreatureMove(const Creature* creature, const Position& oldPos, const Position& newPos);
 	virtual void onCreatureSay(const Creature* creature, SpeakClasses, const std::string& text);
 	virtual void onThink();
-	
+
 private:
 	NpcScriptInterface* m_scriptInterface;
-	
+
 	int32_t m_onCreatureAppear;
 	int32_t m_onCreatureDisappear;
 	int32_t m_onCreatureMove;
@@ -114,18 +114,18 @@ public:
 	virtual const Npc* getNpc() const {return this;};
 
 	virtual bool isPushable() const { return false;};
-	
-	virtual uint32_t idRange(){ return 0x30000000;}
+
+	virtual uint32_t idRange(){ return 0x80000000;}
 	static AutoList<Npc> listNpc;
 	void removeList() {listNpc.removeList(getID());}
 	void addList() {listNpc.addList(this);}
-	
+
 	virtual bool canSee(const Position& pos) const;
-	
+
 	void speak(const std::string& text){};
 	virtual const std::string& getName() const {return name;};
 	virtual const std::string& getNameDescription() const {return name;};
-	
+
 	void doSay(std::string msg);
 	void doMove(Direction dir);
 	void doTurn(Direction dir);
@@ -135,7 +135,7 @@ public:
 	void setCreatureFocus(Creature* creature);
 
 	NpcScriptInterface* getScriptInterface();
-	
+
 protected:
 	virtual void onAddTileItem(const Position& pos, const Item* item);
 	virtual void onUpdateTileItem(const Position& pos, uint32_t stackpos,
@@ -153,12 +153,12 @@ protected:
 	virtual void onCreatureChangeOutfit(const Creature* creature, const Outfit_t& outfit);
 	virtual void onThink(uint32_t interval);
 	virtual std::string getDescription(int32_t lookDistance) const;
-	
+
 	bool isImmune(CombatType_t type) const {return true;}
 	bool isImmune(ConditionType_t type) const {return true;}
 	virtual bool isAttackable() const { return attackable; }
 	virtual bool getNextStep(Direction& dir);
-	
+
 	bool canWalkTo(const Position& fromPos, Direction dir);
 	bool getRandomStep(Direction& dir);
 
@@ -170,12 +170,12 @@ protected:
 	bool floorChange;
 	bool attackable;
 	uint32_t focusCreature;
-	
+
 	NpcEventsHandler* m_npcEventHandler;
 	bool loaded;
-	
+
 	static NpcScriptInterface* m_scriptInterface;
-	
+
 	friend class NpcScriptInterface;
 };
 
