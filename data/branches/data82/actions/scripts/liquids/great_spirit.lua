ea10629cde8818802c6e6dbeb2b8f68571fc7f14
pedro_b <piterb@users.sourceforge.net>
2008-06-28 17:54:22 +0000

    * Added new potions and doors. * Small change on items.otb so new doors can be used as house doors.

--- data/branches/data82/actions/scripts/liquids/great_spirit.lua
+++ data/branches/data82/actions/scripts/liquids/great_spirit.lua
@@ -1,6 +1,20 @@
-local HEALTH_REGEN = 200, 400 --min 200, max 400
-local MANA_REGEN = 100, 200 --min 100, max 200
+local HEALTH_REGEN = {200, 400} --min 200, max 400
+local MANA_REGEN = {100, 200} --min 100, max 200
 local EMPTY_POTION = 7635
+
+local combatHealth = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_HEALING)
+setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
+setCombatParam(combat, COMBAT_PARAM_TARGETCASTERORTOPMOST, TRUE)
+setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, FALSE)
+setCombatParam(combat, COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
+setCombatFormula(combat, COMBAT_FORMULA_DAMAGE, HEALTH_REGEN[1], 0, HEALTH_REGEN[2], 0)
+
+local combatMana = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_MANADRAIN)
+setCombatParam(combat, COMBAT_PARAM_TARGETCASTERORTOPMOST, TRUE)
+setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, FALSE)
+setCombatFormula(combat, COMBAT_FORMULA_DAMAGE, MANA_REGEN[1], 0, MANA_REGEN[2], 0)
 
 local exhaust = createConditionObject(CONDITION_EXHAUSTED)
 setConditionParam(exhaust, CONDITION_PARAM_TICKS, getConfigInfo('exhausted'))
@@ -20,13 +34,12 @@ function onUse(cid, item, frompos, item2, topos)
 		return TRUE
 	end
 
-	if(doPlayerAddHealth(item2.uid, math.random(HEALTH_REGEN)) == LUA_ERROR or
-		doPlayerAddMana(item2.uid, math.random(MANA_REGEN)) == LUA_ERROR) then
+	local var = numberToVariant(item2.uid)
+	if(doCombat(cid, combatHealth, var) == LUA_ERROR or doCombat(cid, combatMana, var) == LUA_ERROR) then
 		return FALSE
 	end
 
 	doAddCondition(cid, exhaust)
-	doSendMagicEffect(getThingPos(item2.uid), CONST_ME_MAGIC_BLUE)
 	doCreatureSay(item2.uid, "Aaaah...", TALKTYPE_ORANGE_1) 
 	doTransformItem(item.uid, EMPTY_POTION)
 	return TRUE
