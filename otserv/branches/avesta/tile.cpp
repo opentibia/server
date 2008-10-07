1dcffac0046eb4f9c7fa4740d2a3784708c019ab
Hampus Joakim Nilsson <mail@hjnilsson.com>
2008-10-07 13:57:18 +0000

    *Fixed bug where you could throw items anywhere.

--- otserv/branches/avesta/tile.cpp
+++ otserv/branches/avesta/tile.cpp
@@ -614,7 +614,7 @@ ReturnValue Tile::__queryRemove(const Thing* thing, uint32_t count) const
 Cylinder* Tile::__queryDestination(int32_t& index, const Thing* thing, Item** destItem,
 	uint32_t& flags)
 {
-	Tile* destTile = this;
+	Tile* destTile = NULL;
 	*destItem = NULL;
 
 	if(floorChangeDown()){
@@ -632,9 +632,7 @@ Cylinder* Tile::__queryDestination(int32_t& index, const Thing* thing, Item** de
 				dx -= 1;
 			if(downTile->floorChange(WEST))
 				dx += 1;
-			downTile = g_game.getTile(dx, dy, dz);
-			if(downTile)
-				destTile = downTile;
+			destTile = g_game.getTile(dx, dy, dz);
 		}
 	}
 	else if(floorChange()){
@@ -642,13 +640,13 @@ Cylinder* Tile::__queryDestination(int32_t& index, const Thing* thing, Item** de
 		int dy = getTilePosition().y;
 		int dz = getTilePosition().z - 1;
 
-		if(destTile->floorChange(NORTH))
+		if(floorChange(NORTH))
 			dy -= 1;
-		if(destTile->floorChange(SOUTH))
+		if(floorChange(SOUTH))
 			dy += 1;
-		if(destTile->floorChange(EAST))
+		if(floorChange(EAST))
 			dx += 1;
-		if(destTile->floorChange(WEST))
+		if(floorChange(WEST))
 			dx -= 1;
 		destTile = g_game.getTile(dx, dy, dz);
 	}
