cdbe95154b9ee086ad38f8a01b4575de48507a90
Hampus Joakim Nilsson <mail@hjnilsson.com>
2008-10-25 13:27:00 +0000

    *Fixed being able to walk up a floor without support under the target tile ( http://otfans.net/project.php?issueid=245 )

--- otserv/branches/avesta/game.cpp
+++ otserv/branches/avesta/game.cpp
@@ -813,8 +813,11 @@ ReturnValue Game::internalMoveCreature(Creature* creature, Direction direction, 
 			if(tmpTile == NULL || (tmpTile->ground == NULL && !tmpTile->hasProperty(BLOCKSOLID))){
 				tmpTile = map->getTile(destPos.x, destPos.y, destPos.z - 1);
 				if(tmpTile && tmpTile->ground && !tmpTile->hasProperty(BLOCKSOLID)){
-					flags = flags | FLAG_IGNOREBLOCKITEM | FLAG_IGNOREBLOCKCREATURE;
-					destPos.z -= 1;
+					Tile* belowTile = map->getTile(destPos.x, destPos.y, destPos.z);
+					if(belowTile == NULL || belowTile->hasProperty(IMMOVABLEBLOCKSOLID)) {
+						flags = flags | FLAG_IGNOREBLOCKITEM | FLAG_IGNOREBLOCKCREATURE;
+						destPos.z -= 1;
+					}
 				}
 			}
 		}
@@ -822,11 +825,14 @@ ReturnValue Game::internalMoveCreature(Creature* creature, Direction direction, 
 			//try go down
 			Tile* tmpTile = map->getTile(destPos);
 			if(currentPos.z != 7 && (tmpTile == NULL || (tmpTile->ground == NULL && !tmpTile->hasProperty(BLOCKSOLID)))){
-				tmpTile = map->getTile(destPos.x, destPos.y, destPos.z + 1);
+				Tile* belowTile = map->getTile(currentPos.x, currentPos.y, currentPos.z + 1);
+				if(belowTile == NULL || belowTile->hasProperty(IMMOVABLEBLOCKSOLID)) {
+					tmpTile = map->getTile(destPos.x, destPos.y, destPos.z + 1);
 
-				if(tmpTile && tmpTile->hasHeight(3)){
-					flags = flags | FLAG_IGNOREBLOCKITEM | FLAG_IGNOREBLOCKCREATURE;
-					destPos.z += 1;
+					if(tmpTile && tmpTile->hasHeight(3)){
+						flags = flags | FLAG_IGNOREBLOCKITEM | FLAG_IGNOREBLOCKCREATURE;
+						destPos.z += 1;
+					}
 				}
 			}
 		}
