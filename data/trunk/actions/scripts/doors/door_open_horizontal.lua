905d2a542b6c613d364b49222cff18435c738ccd
Johannes Rosenqvist <xeroc81@gmail.com>
2008-11-11 21:20:54 +0000

    * Door fixes

--- data/trunk/actions/scripts/doors/door_open_horizontal.lua
+++ data/trunk/actions/scripts/doors/door_open_horizontal.lua
@@ -1,6 +1,6 @@
 function onUse(cid, item, frompos, item2, topos)
 	-- Get the tile to move the things on the door to
-	local nextTile = {x=frompos.x+1, y=frompos.y, z=frompos.z}
+	local nextTile = {x=frompos.x, y=frompos.y+1, z=frompos.z}
 
 	-- Move all moveable things to the next tile
 	doRelocate(frompos, nextTile)
