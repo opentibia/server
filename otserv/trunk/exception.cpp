ab34b9fa99bde2c7685a35e6f904c025f8a6af53
mips <mips_act@users.sourceforge.net>
2009-01-09 21:52:58 +0000

    * Memory was reported wrong in system with more than 2GB.

--- otserv/trunk/exception.cpp
+++ otserv/trunk/exception.cpp
@@ -201,11 +201,15 @@ __cdecl _SEHHandler(
 
 	//system and process info
 	//- global memory information
-	MEMORYSTATUS mstate;
-	GlobalMemoryStatus(&mstate);
-	*outdriver << "Memory load: " << mstate.dwMemoryLoad << std::endl <<
-		"Total phys: " << mstate.dwTotalPhys/1024 << " K available phys: " <<
-		mstate.dwAvailPhys/1024 << " K" << std::endl;
+	MEMORYSTATUSEX mstate;
+	if(GlobalMemoryStatusEx(&mstate)){
+		*outdriver << "Memory load: " << mstate.dwMemoryLoad << std::endl <<
+			"Total phys: " << mstate.ullTotalPhys/1024 << " K available phys: " <<
+			mstate.ullAvailPhys/1024 << " K" << std::endl;
+	}
+	else{
+		*outdriver << "Memory load: Error" << std::endl;
+	}
 	//-process info
 	FILETIME FTcreation,FTexit,FTkernel,FTuser;
 	SYSTEMTIME systemtime;
