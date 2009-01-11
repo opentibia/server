6049d87672f4abab27a7ebd6793b89212eaef709
Hampus Joakim Nilsson <mail@hjnilsson.com>
2009-01-11 21:10:49 +0000

    *Merged data/840 into trunk *Also updated OTB

---
+++ data/trunk/spells/scripts/support/blood_rage.lua
@@ -0,0 +1,18 @@
+
+local conditionAttrib = createConditionObject(CONDITION_ATTRIBUTES)
+
+setConditionParam(conditionAttrib, CONDITION_PARAM_TICKS, 10000)
+setConditionParam(conditionAttrib, CONDITION_PARAM_SKILL_SHIELDPERCENT, -100)
+setConditionParam(conditionAttrib, CONDITION_PARAM_SKILL_MELEEPERCENT, 35)
+
+local combat = createCombatObject()
+setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
+setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
+setCombatCondition(combat, conditionAttrib)
+ 
+function onCastSpell(cid, var)
+	if(doCombat(cid, combat, var) == LUA_NO_ERROR) then
+		return LUA_NO_ERROR
+	end
+	return LUA_ERROR
+end
