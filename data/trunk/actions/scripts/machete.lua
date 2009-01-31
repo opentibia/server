3db45e5454524101f05c608a5f8ce866b154bf80
Hampus Joakim Nilsson <mail@hjnilsson.com>
2009-01-31 15:12:19 +0000

    *Fixes to data folder, as per rafaelhamdan's instructions. :)

--- data/trunk/actions/scripts/machete.lua
+++ data/trunk/actions/scripts/machete.lua
@@ -1,5 +1,10 @@
 function onUse(cid, item, frompos, item2, topos)
-	if (isInArray(JUNGLE_GRASS, item2.itemid) == TRUE) then
+	if (isInArray(JUNGLE_GRASS_REMOVE, item2.itemid) == TRUE) then
+		doRemoveItem(item2.uid)
+		return TRUE
+	end
+	
+	if (isInArray(JUNGLE_GRASS_TRANSFORM, item2.itemid) == TRUE) then
 		doTransformItem(item2.uid, item2.itemid - 1)
 		doDecayItem(item2.uid)
 		return TRUE
