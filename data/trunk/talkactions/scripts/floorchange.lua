6504785e2b5e6cc91774ebb4a1808040e894c462
Rafael Hamdan <rafaelhamdan@users.sourceforge.net>
2009-02-28 23:01:41 +0000

    * Added /up and /down talkactions. You can also choose how many floors you will go down/up using: /up 2, /down 3.

---
+++ data/trunk/talkactions/scripts/floorchange.lua
@@ -0,0 +1,27 @@
+function onSay(cid, words, param)
+	-- Check access
+	if getPlayerAccess(cid) < 2 then
+		return TRUE
+	end
+
+	-- Get number of floors
+	local nfloors = 1
+	if(param ~= "" and tonumber(param)) then
+		nfloors = math.max(0, tonumber(param))
+	end
+
+	-- Change position
+	oldPos = getPlayerPosition(cid)
+	newPos = getPlayerPosition(cid)
+	if(words == "/up") then
+		newPos.z = newPos.z - nfloors
+	else
+		newPos.z = newPos.z + nfloors
+	end
+	if(doTeleportThing(cid, newPos) ~= LUA_ERROR and nfloors > 0) then
+		doSendMagicEffect(oldPos, CONST_ME_POFF)
+		doSendMagicEffect(newPos, CONST_ME_TELEPORT)
+	end
+
+	return FALSE
+end
