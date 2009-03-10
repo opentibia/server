011caabbb72760dfbf9a0da0a38d8f06495b1133
Hampus Joakim Nilsson <mail@hjnilsson.com>
2009-03-10 21:47:55 +0000

    *Small formatting fix to --help (thanks OsoSangre)

--- otserv/trunk/otserv.cpp
+++ otserv/trunk/otserv.cpp
@@ -303,26 +303,25 @@ bool parseCommandLine(CommandLineOptions& opts, std::vector<std::string> args)
 			}
 			opts.errfile = *argi;
 		}
-		else if(arg == "--help"){
-			std::cout <<
-			"Usage: otserv {-i|-p|-c|-r|-l}\n"
-			"\n"
-			"\t-i, --ip $1\t\tIP of gameworld server. Should be equal to the \n"
-			"\t\t\t\tglobal IP.\n"
-			"\t-p, --port $1\t\tPort for server to listen on.\n"
-			"\t-c, --config $1\t\tAlternate config file path.\n"
-			"\t-l, --log-file $1 $2\tAll standard output will be logged to the\n";
-#if !defined(__WINDOWS__)
-			std::cout << "\t-r, --run-file $1\t\tSpecifies a runfile. Will contain the pid\n"
-			"\t\t\t\tof the server process as long as it is running (UNIX).\n";
-#endif
-			std::cout << "\t\t\t\t$1 file, all errors will be logged to $2.\n"
-			"\t--truncate-log\t\tReset log file each time the server is \n"
-			"\t\t\t\tstarted.\n";
-			return false;
+		else if(arg == "--help"){ 
+			std::cout << 
+			"Usage: otserv {-i|-p|-c|-r|-l}\n" 
+			"\n" 
+			"\t-i, --ip $1\t\tIP of gameworld server. Should be equal to the \n" 
+			"\t\t\t\tglobal IP.\n" 
+			"\t-p, --port $1\t\tPort for server to listen on.\n" 
+			"\t-c, --config $1\t\tAlternate config file path.\n" 
+			"\t-l, --log-file $1 $2\tAll standard output will be logged to the $1\n"
+			"\t\t\t\tfile, all errors will be logged to $2.\n"; 
+			#if !defined(__WINDOWS__) 
+			std::cout << "\t-r, --run-file $1\tSpecifies a runfile. Will contain the pid\n" 
+			"\t\t\t\tof the server process as long as it is running \n\t\t\t\t(UNIX).\n"; 
+			#endif 
+			"\t--truncate-log\t\tReset log file each time the server is \n" 
+			"\t\t\t\tstarted.\n"; 
+			return false; 
 		}
-		else
-		{
+		else{
 			std::cout << "Unrecognized command line argument '" << arg << "'\n"
 			"Usage: otserv {-i|-p|-c|-r|-l}" << "\n";
 			return false;
