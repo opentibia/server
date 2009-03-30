f99a2a9f8f10287c49f7ad1f9da85312d2411160
Mathäus Mendel <contato@mathausmendel.com>
2009-03-30 15:56:17 +0000

    * Fixed enchants (who knows how is the value to the rainbow shields, change, please)

--- data/trunk/actions/scripts/enchant.lua
+++ data/trunk/actions/scripts/enchant.lua
@@ -5,24 +5,26 @@ local REAGENT_EARTH  = 7761
 
 function onUse(cid, item, frompos, item2, topos)
 
+	local count = ENCHANTABLE_THINGS[item2.itemid][5]
+
 	if ENCHANTABLE_THINGS[item2.itemid] == nil then
 		return FALSE
 	end
 
 	if (item.itemid == REAGENT_FIRE) then
-		doTransformItem(item2.uid, ENCHANTABLE_THINGS[item2.itemid][1], 1000)
+		doTransformItem(item2.uid, ENCHANTABLE_THINGS[item2.itemid][1], count)
 		doRemoveItem(item.uid, 1)
 
 	elseif (item.itemid == REAGENT_ICE) then
-		doTransformItem(item2.uid, ENCHANTABLE_THINGS[item2.itemid][2], 1000)
+		doTransformItem(item2.uid, ENCHANTABLE_THINGS[item2.itemid][2], count)
 		doRemoveItem(item.uid, 1)
 
 	elseif (item.itemid == REAGENT_ENERGY) then
-		doTransformItem(item2.uid, ENCHANTABLE_THINGS[item2.itemid][3], 1000)
+		doTransformItem(item2.uid, ENCHANTABLE_THINGS[item2.itemid][3], count)
 		doRemoveItem(item.uid, 1)
 
 	elseif (item.itemid == REAGENT_EARTH) then
-		doTransformItem(item2.uid, ENCHANTABLE_THINGS[item2.itemid][4], 1000)
+		doTransformItem(item2.uid, ENCHANTABLE_THINGS[item2.itemid][4], count)
 		doRemoveItem(item.uid, 1)
 	else
 		return FALSE
