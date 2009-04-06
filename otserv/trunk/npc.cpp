be59841fcb136c0b5e4e2fa5de939dfa71ed817a
Fighting Elf <fightingelf@users.sourceforge.net>
2009-04-06 23:40:15 +0000

    * Ab'dendriel? Now possible!

--- otserv/trunk/npc.cpp
+++ otserv/trunk/npc.cpp
@@ -2236,7 +2236,7 @@ uint32_t Npc::getMatchCount(NpcResponse* response, std::vector<std::string> word
 			else{
 				std::vector<std::string>::iterator wordIter = wordList.end();
 				for(wordIter = lastWordMatchIter; wordIter != wordList.end(); ++wordIter){
-					size_t pos = (*wordIter).find_first_of("!\"#�%&/()=?`{[]}\\^*><,.-_'~");
+					size_t pos = (*wordIter).find_first_of("!\"#�%&/()=?`{[]}\\^*><,.-_~");
 					if(pos == std::string::npos){
 						pos = 0;
 					}
