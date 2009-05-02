4944ad0edd4a15511c0363b95da1fb0d59ec87b5
Johannes Rosenqvist <xeroc81@gmail.com>
2009-05-02 20:01:02 +0000

    * ?

---
+++ data/trunk/movements/scripts/doors/door_close_vertical.lua
@@ -0,0 +1,29 @@
+function onStepOut(cid, item, pos)
+	if(item.actionid == 0) then
+		-- This is not a special door
+		return TRUE
+	end
+
+	local topos = {x=pos.x+1, y=pos.y, z=pos.z}
+	doRelocate(pos, topos)
+
+	-- Remove any item that was not moved
+	-- Happens when there is an unmoveable item on the door, ie. a fire field
+	local tmpPos = {x=pos.x, y=pos.y, z=pos.z, stackpos=-1}
+	local tileCount = getTileThingByPos(tmpPos)
+	local i = 1
+	local tmpItem = {uid = 1}
+
+	while(tmpItem.uid ~= 0 and i < tileCount) do
+		tmpPos.stackpos = i
+		tmpItem = getTileThingByPos(tmpPos)
+		if(tmpItem.uid ~= item.uid and tmpItem.uid ~= 0) then
+			doRemoveItem(tmpItem.uid)
+		else
+			i = i + 1
+		end
+	end
+
+	doTransformItem(item.uid, item.itemid-1)
+	return TRUE
+end
