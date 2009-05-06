690d81e5090a0b2b369609854a081a1ed7f36fd2
Rafael Hamdan <rafaelhamdan@users.sourceforge.net>
2009-05-06 19:34:02 +0000

    * Formatted /serverinfo.

--- data/trunk/talkactions/scripts/serverinfo.lua
+++ data/trunk/talkactions/scripts/serverinfo.lua
@@ -11,19 +11,19 @@ local config = {
 function onSay(cid, words, param)
 	-- Add basic info
 	local str = "Server Information:\n\n"
-	str = str .. "Experience Rate: " .. config.rate_exp .. "\n"
-	str = str .. "Magic Rate: " .. config.rate_mag .. "\n"
-	str = str .. "Skill Rate: " .. config.rate_skill .. "\n"
-	str = str .. "Loot Rate: " .. config.rate_loot
+	str = str .. "Experience Rate: " .. config.rate_exp .. "x\n"
+	str = str .. "Magic Rate: " .. config.rate_mag .. "x\n"
+	str = str .. "Skill Rate: " .. config.rate_skill .. "x\n"
+	str = str .. "Loot Rate: " .. config.rate_loot .. "x"
 	
 	-- Add house renting info
 	if(config.rent_period ~= "never") then
-		str = str .. "\nHouse Rent: House rents are paid " .. config.rent_period
+		str = str .. "\nHouse Rent: House rents are paid " .. config.rent_period .. "."
 	end
 
 	-- Add banishment by killing info
 	if(config.frags_to_ban ~= 0) then
-		str = str .. "\nKilling Banishment: You will be banished if you get " .. config.frags_to_ban .. "frags or more."
+		str = str .. "\nKilling Banishment: You will be banished if you get " .. config.frags_to_ban .. " frags or more."
 	end	
 
 	doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, str)
