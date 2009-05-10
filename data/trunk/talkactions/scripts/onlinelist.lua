fe8f20a93d3a07ba2f10a1ab5890ed922c9cee7c
Rafael Hamdan <rafaelhamdan@users.sourceforge.net>
2009-05-10 18:01:06 +0000

    * Fixed !online commands (now it works perfectly). * Formatted !frags file talkaction name.

--- data/trunk/talkactions/scripts/onlinelist.lua
+++ data/trunk/talkactions/scripts/onlinelist.lua
@@ -1,17 +1,24 @@
 function onSay(cid, words, param)
 	local onlineList = getPlayersOnlineList()
-
+	
+	doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, #onlineList .. " Players Online")
+	
 	local str = ""
 	for i, uid in ipairs(onlineList) do
-		if(i == 1) then
-			str = "Players Online: " .. getPlayerName(uid)
+		local name = getPlayerName(uid)
+		if string.len(name) + string.len(str) > 255 then
+			doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, str)
+			str = name
+		elseif i == 1 then
+			str = name
 		else
-			str = str .. ", " .. getPlayerName(uid)
+			str = str .. ", " .. name
 		end
 	end
-	str = str .. ".\nTotal: " .. #onlineList .. " players."
 
-	doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, str)
-	doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_RED, words)
+	if str ~= "" then
+		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, str)
+	end
+
 	return FALSE
 end
