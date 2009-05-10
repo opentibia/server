7e824f33cb5fa6a2aaee0f83df8c25a97e8b036d
Rafael Hamdan <rafaelhamdan@users.sourceforge.net>
2009-05-10 19:52:28 +0000

    * Firewalker boots protects against Fire Field, while we do not have the right attribute, we should not set fire protection. * Fixed !online. Players with SpecialVip flag won't be shown in the list.

--- data/trunk/talkactions/scripts/onlinelist.lua
+++ data/trunk/talkactions/scripts/onlinelist.lua
@@ -1,23 +1,39 @@
 function onSay(cid, words, param)
 	local onlineList = getPlayersOnlineList()
 	
-	doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, #onlineList .. " Players Online")
+	doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Players Online:")
 	
 	local str = ""
+	local j = 1
+	local k = 0
 	for i, uid in ipairs(onlineList) do
-		local name = getPlayerName(uid)
-		if string.len(name) + string.len(str) > 255 then
-			doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, str)
-			str = name
-		elseif i == 1 then
-			str = name
+		--Player is not shown in list if he has SpecialVip flag
+		if getPlayerFlagValue(uid, math.pow(2, 30)) == FALSE then
+			local name = getPlayerName(uid)
+			if string.len(name) + string.len(str) > 255 then
+				doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, str)
+				str = name
+			elseif j == 1 then
+				str = name
+			else
+				str = str .. ", " .. name
+			end
+			j = j + 1
 		else
-			str = str .. ", " .. name
-		end
+			k = k + 1
+		end	
 	end
 
 	if str ~= "" then
+		str = str .. "."
 		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, str)
+	end
+
+	local total = #onlineList - k
+	if total == 1 then
+		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Total: " .. total .. " player online.")
+	else
+		doPlayerSendTextMessage(cid, MESSAGE_STATUS_CONSOLE_BLUE, "Total: " .. total .. " players online.")
 	end
 
 	return FALSE
