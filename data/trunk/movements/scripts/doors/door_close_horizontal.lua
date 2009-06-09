376dc2678800849e9bbc69a236cf08b96b6c4bbe
Johannes Rosenqvist <xeroc81@gmail.com>
2009-06-09 22:28:49 +0000

    * door fixes

--- data/trunk/movements/scripts/doors/door_close_horizontal.lua
+++ data/trunk/movements/scripts/doors/door_close_horizontal.lua
@@ -1,27 +1,15 @@
-function onStepOut(cid, item, pos)
+function onStepOut(cid, item, topos, frompos)
 	if(item.actionid == 0) then
 		-- This is not a special door
 		return TRUE
 	end
 
-	local topos = {x=pos.x, y=pos.y+1, z=pos.z}
-	doRelocate(pos, topos)
+	local movepos = {x=frompos.x, y=frompos.y+1, z=frompos.z}
+	doRelocate(frompos, movepos)
 
-	-- Remove any item that was not moved
-	-- Happens when there is an unmoveable item on the door, ie. a fire field
-	local tmpPos = {x=pos.x, y=pos.y, z=pos.z, stackpos=-1}
-	local tileCount = getTileThingByPos(tmpPos)
-	local i = 1
-	local tmpItem = {uid = 1}
-
-	while(tmpItem.uid ~= 0 and i < tileCount) do
-		tmpPos.stackpos = i
-		tmpItem = getTileThingByPos(tmpPos)
-		if(tmpItem.uid ~= item.uid and tmpItem.uid ~= 0) then
-			doRemoveItem(tmpItem.uid)
-		else
-			i = i + 1
-		end
+	local field = getTileItemByType(frompos, ITEM_TYPE_MAGICFIELD)
+	if(field.itemid ~=0) then
+		doRemoveItem(field.uid)
 	end
 
 	doTransformItem(item.uid, item.itemid-1)
