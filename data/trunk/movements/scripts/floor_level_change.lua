5837f399ab81794f5c4876ed0e339013bda98441
Johannes Rosenqvist <xeroc81@gmail.com>
2009-09-02 15:58:59 +0000

    * East/South stair support

---
+++ data/trunk/movements/scripts/floor_level_change.lua
@@ -0,0 +1,23 @@
+function onStepIn(cid, item, pos, frompos)
+	if frompos.x < pos.x then
+		local newpos = pos
+		newpos.x = newpos.x + 1
+		doTeleportThing(cid, newpos)
+	elseif frompos.x > pos.x then
+		local newpos = pos
+		newpos.z = newpos.z + 1
+		newpos.x = newpos.x - 2
+		doTeleportThing(cid, newpos)
+	elseif frompos.y < pos.y then
+		local newpos = pos
+		newpos.y = newpos.y + 1
+		doTeleportThing(cid, newpos)
+	elseif frompos.y > pos.y then
+		local newpos = pos
+		newpos.z = newpos.z + 1
+		newpos.y = newpos.y - 2
+		doTeleportThing(cid, newpos)
+	end
+
+	return TRUE
+end
