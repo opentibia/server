fe241e46ebfba92de412214942ebc1519ed449d1
Hampus Joakim Nilsson <mail@hjnilsson.com>
2009-10-13 22:53:22 +0000

    *Removed debug messages from mailbox script.

--- otserv/branches/revscriptsys2/data/scripts/otstd/actions/mailbox.lua
+++ otserv/branches/revscriptsys2/data/scripts/otstd/actions/mailbox.lua
@@ -7,17 +7,14 @@ otstd.mailboxes = {
 	}
 
 function otstd.mailbox.handler(event)
-	event.creature:sendNote("Attempting to send")
 	local mail = event.item
 	local label = nil
 	
 	if mail:getItemID() == 2595 then
 		label = filter((function(i) return i:getItemID() == 2599 end), mail:getItems())
 		if #label == 0 then
-			event.creature:sendNote("No label found")
 			return
 		end
-		event.creature:sendNote("Found a label " .. #label)
 		label = label[1]
 	end
 	
@@ -27,16 +24,13 @@ function otstd.mailbox.handler(event)
 	
 	if not label then
 		-- Not a mail item
-		event.creature:sendNote("No label")
 		return
 	end
-	event.creature:sendNote("Found label")
 	
 	local text = label:getText()
 	
 	local lines = text:explode()
 	if #lines < 2 then
-		local s = "label too short:"
 		for k, v in pairs(lines) do
 			s = s .. k .. '=' .. v .. '  '
 		end
@@ -47,11 +41,9 @@ function otstd.mailbox.handler(event)
 	local town = map:getTown(lines[2]:strip_whitespace())
 	
 	if not town then
-		event.creature:sendNote("No town specified")
 		return
 	end
 	
-	event.creature:sendNote("Sending mail")
 	sendMail(mail, name, town)
 end
 
