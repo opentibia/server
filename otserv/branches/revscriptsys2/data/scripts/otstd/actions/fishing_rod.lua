2c610ad68f20aced13bfcc2c4257725f2f51e11d
Hampus Joakim Nilsson <mail@hjnilsson.com>
2009-10-14 18:27:42 +0000

    *Removed completely useless extra variable, return value already indicates if we caught something.

--- otserv/branches/revscriptsys2/data/scripts/otstd/actions/fishing_rod.lua
+++ otserv/branches/revscriptsys2/data/scripts/otstd/actions/fishing_rod.lua
@@ -20,10 +20,8 @@ otstd.fishing.rods = {
 				if event.hasFish and roll > 0.7 then
 					local fish = createItem(10224)
 					player:addItem(fish)
-					event.caughtFish = true
 					return true
 				end
-				event.caughtFish = false
 				return true
 			end
 		},
@@ -103,7 +101,6 @@ function otstd.fishing.standardRodHandler(event)
 	if event.hasFish and roll > 0.7 then
 		local fish = createItem(event.spot:getFish(roll))
 		player:addItem(fish)
-		event.caughtFish = true
 		return true -- Return true if we caught something
 	end
 	return false
@@ -131,15 +128,12 @@ function otstd.fishing.handler(event)
 				event.hasFish = false
 			end
 			
-			event.caughtFish = false
 			if event.rod.handler and event.rod.handler(event) or otstd.fishing.standardRodHandler(event) then
-				if event.caughtFish then
-					player:advanceSkill(SKILL_FISH, 2)
-					player:removeItem(event.rod.bait, -1, 1)
-				
-					spot:setItemID(spotdata.newid)
-					spot:startDecaying()
-				end
+				player:advanceSkill(SKILL_FISH, 2)
+				player:removeItem(event.rod.bait, -1, 1)
+			
+				spot:setItemID(spotdata.newid)
+				spot:startDecaying()
 			elseif event.hasFish then
 				-- Didn't catch anything, but still a noble try (used a bait)...
 				player:advanceSkill(SKILL_FISH, 1)
