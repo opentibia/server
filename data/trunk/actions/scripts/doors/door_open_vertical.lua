07a57aaaa8f11d694f766d637900d256c9d79568
Assassina Mutante <assassina@users.sourceforge.net>
2010-02-12 15:11:24 +0000

    http://otfans.net/project.php?issueid=720

--- data/trunk/actions/scripts/doors/door_open_vertical.lua
+++ data/trunk/actions/scripts/doors/door_open_vertical.lua
@@ -1,16 +1,14 @@
 function onUse(cid, item, frompos, item2, topos)
-	if item.actionid == 0 then
-		-- Get the tile to move the things on the door to
-		local nextTile = {x=frompos.x+1, y=frompos.y, z=frompos.z}
+	-- Get the tile to move the things on the door to
+	local nextTile = {x=frompos.x+1, y=frompos.y, z=frompos.z}
 
-		-- Move all moveable things to the next tile
-		doRelocate(frompos, nextTile)
+	-- Move all moveable things to the next tile
+	doRelocate(frompos, nextTile)
 
-		-- Transform the door
-		-- doRelocate can trigger other scripts (stepOut) so the uid might be invalid
-		if(isValidUID(item.uid)) then
-			doTransformItem(item.uid, item.itemid-1)
-		end
+	-- Transform the door
+	-- doRelocate can trigger other scripts (stepOut) so the uid might be invalid
+	if(isValidUID(item.uid)) then
+		doTransformItem(item.uid, item.itemid-1)
 	end
 
 	return TRUE
