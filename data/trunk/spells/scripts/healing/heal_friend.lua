2b3d0e137c2ccb71cf9a98af545b5cbedebbf3ea
Rafael Hamdan <rafaelhamdan@users.sourceforge.net>
2010-03-24 01:27:20 +0000

    * http://otfans.net/project.php?issueid=794

--- data/trunk/spells/scripts/healing/heal_friend.lua
+++ data/trunk/spells/scripts/healing/heal_friend.lua
@@ -1,6 +1,5 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_HEALING)
-setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, FALSE)
 setCombatParam(combat, COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
 
@@ -13,5 +12,17 @@ end
 setCombatCallback(combat, CALLBACK_PARAM_LEVELMAGICVALUE, "onGetFormulaValues")
 
 function onCastSpell(cid, var)
-	return doCombat(cid, combat, var)
+	local ret = doCombat(cid, combat, var)
+
+	--send effects
+	if ret == LUA_NO_ERROR then
+		doSendMagicEffect(getCreaturePosition(cid), CONST_ME_MAGIC_BLUE)
+		if isCreature(var.number) == TRUE then
+			if cid ~= var.number then
+				doSendMagicEffect(getCreaturePosition(var.number), CONST_ME_MAGIC_GREEN)
+			end
+		end
+	end
+
+	return ret
 end
