64a9d8f187002300bb5ff35543dbff6a9b603d4a
Assassina Mutante <assassina@users.sourceforge.net>
2010-04-20 09:18:33 +0000

    Ops, I forgot to add the else.lua file at the SVN.

---
+++ data/trunk/actions/scripts/else.lua
@@ -0,0 +1,18 @@
+function isBreakingTool(item)
+  local t = getItemWeaponTypeByUID(item.uid)
+  return (t == WEAPON_SWORD) or (t == WEAPON_CLUB) or (t == WEAPON_AXE)
+end
+
+function onUse(cid, item, frompos, item2, topos)
+	if BREAKABLE_BY_WEAPONS[item2.itemid] ~= nil and isBreakingTool(item) then
+		local chance = BREAKABLE_BY_WEAPONS[item2.itemid].chance
+		if chance == nil then chance = 20 end --default value for chance
+		if math.random(100) <= chance then
+			doTransformItem(item2.uid, BREAKABLE_BY_WEAPONS[item2.itemid].remainings)
+			doDecayItem(item2.uid)
+		end
+		doSendMagicEffect(topos, CONST_ME_POFF)
+		return TRUE
+	end
+	return FALSE
+end
