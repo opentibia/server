9007ee1a9fcb044c2405efd00533912884699306
Assassina Mutante <assassina@users.sourceforge.net>
2010-04-21 09:59:00 +0000

    Solved an issue with string.explode, which was causing http://otfans.net/project.php?issueid=811.

--- data/trunk/functions.lua
+++ data/trunk/functions.lua
@@ -414,7 +414,7 @@ function string.explode(p, d, m)
 	if(#p == 1) then return p end
 		while true do
 			l=string.find(p,d,ll+1,true) -- find the next d in the string
-			if l~=nil and j < limit and limit ~= 0 then -- if "not not" found then..
+			if l~=nil and (j < limit or limit == 0) then -- if "not not" found then..
 				table.insert(t, string.sub(p,ll,l-1)) -- Save it in our array.
 				ll=l+1 -- save just after where we found it for searching next time.
 				j=j+1 -- number of explosions
