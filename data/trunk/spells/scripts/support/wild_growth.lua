2b2207f34983f756aa41bfe53876824b9d397689
Johannes Rosenqvist <xeroc81@gmail.com>
2010-08-25 23:38:09 +0000

    * new ids for magic wall and wild growth (player created walls are not blockable)

--- data/trunk/spells/scripts/support/wild_growth.lua
+++ data/trunk/spells/scripts/support/wild_growth.lua
@@ -1,6 +1,6 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_EARTH)
-setCombatParam(combat, COMBAT_PARAM_CREATEITEM, 1499)
+setCombatParam(combat, COMBAT_PARAM_CREATEITEM, 11099)
 
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
