84fb4b30d997ddc1ff17a9b56b2ed05309881f90
Thomaz Yuji Babá <thomaz.yuji@gmail.com>
2010-09-12 10:15:19 +0000

    * Added a check to see if GM has a flag to become invisible.

--- data/trunk/talkactions/scripts/gminvisible.lua
+++ data/trunk/talkactions/scripts/gminvisible.lua
@@ -1,6 +1,8 @@
 function onSay(cid, words, param)
-	doPlayerToogleGmInvisible(cid)
-	if isGmInvisible(cid) == TRUE then
+	if (isPlayer(cid) == TRUE and getPlayerFlagValue(cid, PlayerFlag_CannotBeSeen) == TRUE) then
+		doPlayerToogleGmInvisible(cid)
+	end
+	if (isGmInvisible(cid) == TRUE) then
 		doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You are now invisible.")
 	else
 		doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, "You are now visible.")
