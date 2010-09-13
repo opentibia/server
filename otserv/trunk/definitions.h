ce094c00bf8d5b5df9d20be8d06aadefcc6b4d8c
Piotr Walaszczyk <pietiatibia@gmail.com>
2010-09-13 12:59:06 +0000

    *Fixed compiling on linux

--- otserv/trunk/definitions.h
+++ otserv/trunk/definitions.h
@@ -80,21 +80,21 @@ enum passwordType_t{
     Compiler setup
 */
 #if defined __GNUC__
-	#include "compiler\gcc.h"
+	#include "compiler/gcc.h"
 	#ifdef __MINGW32__
-		#include "compiler\mingw32.h"
+		#include "compiler/mingw32.h"
 	#elif defined __CYGWIN__
-		#include "compiler\cygwin.h"
+		#include "compiler/cygwin.h"
 	#endif
 #elif defined(_MSC_VER)
-	#include "compiler\msvc.h"
+	#include "compiler/msvc.h"
 #endif
 
 /*
     After setting up the compiler we need to
     solve the differences to make the code portable.
 */
-#include "compiler\workarounds.h"
+#include "compiler/workarounds.h"
 
 /*
     If the compiler supports the upcoming standard,
