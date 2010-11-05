e4e5eda72ad0dccfe9480142baa79d367e611cd5
Assassina Mutante <assassina@users.sourceforge.net>
2010-11-05 08:00:00 +0000

    Ops...

--- data/trunk/spells/scripts/support/wild_growth.lua
+++ data/trunk/spells/scripts/support/wild_growth.lua
@@ -2,8 +2,6 @@ local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_DISTANCEEFFECT, CONST_ANI_EARTH)
 setCombatParam(combat, COMBAT_PARAM_CREATEITEM, ITEM_WILDGROWTH) --if the server is non-pvp the server itself changes to the nonPvP version of the item
 
-setCombatParam(combat, COMBAT_PARAM_CREATEITEM, fieldid)
-
 function onCastSpell(cid, var)
 	return doCombat(cid, combat, var)
 end
