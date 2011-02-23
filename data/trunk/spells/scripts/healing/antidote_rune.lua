22bf3b36b408b44b9baca89d13e889f7032421a7
Assassina Mutante <assassina@users.sourceforge.net>
2011-02-23 00:05:48 +0000

    Solved a typo.

--- data/trunk/spells/scripts/healing/antidote_rune.lua
+++ data/trunk/spells/scripts/healing/antidote_rune.lua
@@ -1,7 +1,7 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
 setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, false)
-setCombatParam(combat, COMBAT_PARAM_TARGETCASTERTOPORMOST, true)
+setCombatParam(combat, COMBAT_PARAM_TARGETCASTERORTOPMOST, true)
 setCombatParam(combat, COMBAT_PARAM_DISPEL, CONDITION_POISON)
 
 function onCastSpell(cid, var)
