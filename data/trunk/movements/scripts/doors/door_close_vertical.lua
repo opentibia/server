35e0ac9c73b2ae410ac9874a33ac8b93235d963d
Piotr Walaszczyk <pietiatibia@gmail.com>
2011-03-20 15:20:59 +0000

    *Fixed second serious critical bug with 100 % cpu usage(http://otfans.net/project.php?issueid=951) (all distros should apply this , even if we will set tile per limit we will still be able to lag the server after some time ...)

--- data/trunk/movements/scripts/doors/door_close_vertical.lua
+++ data/trunk/movements/scripts/doors/door_close_vertical.lua
@@ -1,11 +1,15 @@
 function onStepOut(cid, item, topos, frompos)
+	--WARNING if relocate_items_on_doors will be set to true it might cause serious bug with CPU usage.
+	local relocate_items_on_doors = false
 	if(item.actionid == 0) then
 		-- This is not a special door
 		return true
 	end
 
 	local movepos = {x=frompos.x+1, y=frompos.y, z=frompos.z}
+	if relocate_items_on_doors == true then
 	doRelocate(frompos, movepos)
+	end
 
 	local field = getTileItemByType(frompos, ITEM_TYPE_MAGICFIELD)
 	if(field.itemid ~=0) then
