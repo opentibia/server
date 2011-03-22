2e38239b6eba2948302ec9527cf94be56fd731d2
Assassina Mutante <assassina@users.sourceforge.net>
2011-03-22 05:02:45 +0000

    Included the parameter amount at the usage of the relocate function at door scripts, so we can still mimic the behavior of Cip's servers in all normal situations and avoiding the weakness of a high CPU usage at the same time.

--- data/trunk/movements/scripts/doors/door_close_horizontal.lua
+++ data/trunk/movements/scripts/doors/door_close_horizontal.lua
@@ -1,15 +1,14 @@
 function onStepOut(cid, item, topos, frompos)
-	--WARNING if relocate_items_on_doors will be set to true it might cause serious bug with CPU usage.
-	local relocate_items_on_doors = false
+	--[[WARNING: if maxAmountOfRealocationItens is set into a very high value, players may be able to abuse a 
+                     of a weakness at the engine and cause severe lag at your server --]]
+	local maxAmountOfRealocationItens = 50
 	if(item.actionid == 0) then
 		-- This is not a special door
 		return true
 	end
 
 	local movepos = {x=frompos.x, y=frompos.y+1, z=frompos.z}
-	if relocate_items_on_doors == true then
-	doRelocate(frompos, movepos)
-	end
+	doRelocate(frompos, movepos, false, maxAmountOfRealocationItens)
 
 	local field = getTileItemByType(frompos, ITEM_TYPE_MAGICFIELD)
 	if(field.itemid ~=0) then
