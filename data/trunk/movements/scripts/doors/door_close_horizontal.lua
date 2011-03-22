3ab5562cbfdabb7882c12812b9fb1c3361f58065
Piotr Walaszczyk <pietiatibia@gmail.com>
2011-03-22 06:38:36 +0000

    *It will eventually lag the server after some time, ask me on msn how...

--- data/trunk/movements/scripts/doors/door_close_horizontal.lua
+++ data/trunk/movements/scripts/doors/door_close_horizontal.lua
@@ -1,14 +1,15 @@
 function onStepOut(cid, item, topos, frompos)
-	--[[WARNING: if maxAmountOfRealocationItens is set into a very high value, players may be able to abuse a 
-                     of a weakness at the engine and cause severe lag at your server --]]
-	local maxAmountOfRealocationItens = 50
+	--WARNING if relocate_items_on_doors will be set to true it might cause serious bug with CPU usage.
+	local relocate_items_on_doors = false
 	if(item.actionid == 0) then
 		-- This is not a special door
 		return true
 	end
 
 	local movepos = {x=frompos.x, y=frompos.y+1, z=frompos.z}
-	doRelocate(frompos, movepos, false, maxAmountOfRealocationItens)
+	if relocate_items_on_doors == true then
+	doRelocate(frompos, movepos)
+	end
 
 	local field = getTileItemByType(frompos, ITEM_TYPE_MAGICFIELD)
 	if(field.itemid ~=0) then
