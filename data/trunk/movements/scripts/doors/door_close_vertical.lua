ea284ba24d2f1e16133cdba7d70606db15d83a4e
Assassina Mutante <assassina@users.sourceforge.net>
2011-03-23 05:07:22 +0000

    We better keep it that way, for now. It will protect servers from any possibility of the high CPU weakness and it is better than totally bypass the doRelocate (it might cause problems at some situations - even they are VERY unlikely to happen). Anyway, we still need a better soluction for the issue.

--- data/trunk/movements/scripts/doors/door_close_vertical.lua
+++ data/trunk/movements/scripts/doors/door_close_vertical.lua
@@ -1,15 +1,14 @@
 function onStepOut(cid, item, topos, frompos)
-	--WARNING if relocate_items_on_doors will be set to true it might cause serious bug with CPU usage.
-	local relocate_items_on_doors = false
+	--[[WARNING: if maxAmountOfRealocationItens is set into a very high value, players may be able to abuse  
+                     of a weakness at the engine and cause severe lag at your server --]]
+	local maxAmountOfRealocationItens = 1 --don't change this value until we solve some issues with the engine
 	if(item.actionid == 0) then
 		-- This is not a special door
 		return true
 	end
 
 	local movepos = {x=frompos.x+1, y=frompos.y, z=frompos.z}
-	if relocate_items_on_doors == true then
-	doRelocate(frompos, movepos)
-	end
+	doRelocate(frompos, movepos, false, maxAmountOfRealocationItens)
 
 	local field = getTileItemByType(frompos, ITEM_TYPE_MAGICFIELD)
 	if(field.itemid ~=0) then
