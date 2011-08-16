1466fcdd6ca061ad2bae0d491a9aa9da0a57a0d2
Assassina Mutante <assassina@users.sourceforge.net>
2011-08-16 07:49:15 +0000

    Updated juice squeezer so it now works with lemons too.

--- data/trunk/actions/scripts/juice_squeezer.lua
+++ data/trunk/actions/scripts/juice_squeezer.lua
@@ -1,8 +1,10 @@
 local JUICE_FRUITS = {2673, 2674, 2675, 2676, 2677, 2679, 2680, 2681, 2682, 2683, 2684, 2685}
 local ITEM_COCONUT = 2678
+local ITEM_LEMON = 8841
 local ITEM_VIAL = 11396
 local TYPE_COCONUT_MILK = 14
 local TYPE_JUICE = 21
+local TYPE_LEMONADE = 5
 
 function onUse(cid, item, frompos, item2, topos)
 	if (isInArray(JUICE_FRUITS, item2.itemid) ) then
@@ -13,7 +15,12 @@ function onUse(cid, item, frompos, item2, topos)
 	elseif (item2.itemid == ITEM_COCONUT) then
 		if (doPlayerRemoveItem(cid, ITEM_VIAL, 1, 0) ) then
 			doRemoveItem(item2.uid, 1)
-			doPlayerAddItem(cid,ITEM_VIAL, TYPE_COCONUT_MILK)
+			doPlayerAddItem(cid, ITEM_VIAL, TYPE_COCONUT_MILK)
+		end
+	elseif (item2.itemid == ITEM_LEMON) then
+		if (doPlayerRemoveItem(cid, ITEM_VIAL, 1, 0) ) then
+			doRemoveItem(item2.uid, 1)
+			doPlayerAddItem(cid, ITEM_VIAL, TYPE_LEMONADE)
 		end
 	else
 		return false
