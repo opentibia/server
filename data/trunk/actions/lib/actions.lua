2d102ef803279448b4bf0145289fd057ca04d632
Piotr Walaszczyk <pietiatibia@gmail.com>
2012-02-12 23:25:19 +0000

    *http://otfans.net/project.php?issueid=1001#note6485(still haven't made that each squeezing tool works diffrent)

--- data/trunk/actions/lib/actions.lua
+++ data/trunk/actions/lib/actions.lua
@@ -182,3 +182,142 @@ function useRope(cid, item, frompos, item2, topos)
 	end
 	return true
 end
+
+
+
+local function __doTransformHole__(parameters)
+	local thing = getTileItemById(parameters.pos, MUD_HOLE)
+	local newItem = doTransformItem(thing.uid, parameters.oldType)
+	if parameters.oldaid ~= 0 and newItem then
+		doSetItemActionId(thing.uid, parameters.oldaid)
+	end	
+end
+
+function useShovel(cid, item, frompos, item2, topos)
+local TILE_SAND 		= 	9059
+local ITEM_SCARAB_COIN 		= 	2159
+local TUMB_ENTRANCE		= 	1345
+local SCARAB_TILE		=	101
+local MUD_HOLE		=	489
+local SCARAB_COIN_TILE		= 	102
+local duration = 5 * 60000 -- 5 minutes
+
+	if (isInArray(CLOSED_HOLE, item2.itemid) ) then
+		if item2.itemid == 8579 then
+			doTransformItem(item2.uid, 8585)
+		else
+			doTransformItem(item2.uid, item2.itemid + 1)
+		end
+	elseif (item2.itemid == TILE_SAND) then
+		if (item2.actionid == TUMB_ENTRANCE) then
+			if (math.random(1, 5) == 1) then
+				doTransformItem(item2.uid, MUD_HOLE)
+				addEvent(__doTransformHole__, duration, {oldType = item2.itemid, pos = topos, oldaid = item2.actionid})
+				if item2.actionid ~= 0 then
+					doSetItemActionId(item2.uid, item2.actionid)
+				end
+			end
+		elseif (item2.actionid == SCARAB_TILE) then
+			if (math.random(1, 20) == 1) then
+				doSummonCreature("Scarab", topos)
+				doSetItemActionId(item2.uid, SCARAB_TILE + 2)
+			end
+		elseif (item2.actionid == SCARAB_COIN_TILE) then
+			if (math.random(1, 20) == 1) then
+				doCreateItem(ITEM_SCARAB_COIN, topos)
+				doSetItemActionId(item2.uid, SCARAB_COIN_TILE + 2)
+			end
+		elseif (item2.actionid == SCARAB_TILE + 2) then
+			if (math.random(1, 40) == 1) then
+				doSetItemActionId(item2.uid, SCARAB_TILE)
+			end
+		elseif (item2.actionid == SCARAB_COIN_TILE + 2) then
+			if (math.random(1, 40) == 1) then
+				doSetItemActionId(item2.uid, SCARAB_COIN_TILE)
+			end
+		end
+		doSendMagicEffect(topos, CONST_ME_POFF)
+	else
+		return false
+	end
+
+	doDecayItem(item2.uid)
+	return true
+end
+	
+
+function usePick(cid, item, fromPosition, itemEx, toPosition)
+
+local FRAGILE_ICE	=	7200
+local ICE_FISHHOLE	=	7236
+local duration = 5 * 60000 -- 5 minutes
+
+	local itemGround = getThingFromPos({x = toPosition.x, y = toPosition.y, z = toPosition.z + 1, stackpos = STACKPOS_GROUND})
+	if(isInArray(ROPE_SPOT, itemGround.itemid) and isMoveable(itemEx.uid) == false and isCorpse(itemEx.uid) == false) then
+		doTransformItem(itemEx.uid, MUD_HOLE)
+		doSendMagicEffect(toPosition, CONST_ME_POFF)
+		if itemEx.actionid ~= 0 then
+			doSetItemActionId(itemEx.uid, itemEx.actionid)
+		end
+		addEvent(__doTransformHole__, duration, {oldType = itemEx.itemid, pos = toPosition, oldAid = itemEx.actionid})
+		return true
+	end
+
+	if(itemEx.itemid == FRAGILE_ICE) then
+		doTransformItem(itemEx.uid, ICE_FISHHOLE)
+		if itemEx.actionid ~= 0 then
+			doSetItemActionId(itemEx.uid, itemEx.actionid)
+		end
+		doSendMagicEffect(toPosition, CONST_ME_BLOCKHIT)
+		return true
+	end
+
+	return false
+end
+
+
+function useMachete(cid, item, frompos, item2, topos)
+	local ret = false
+
+	if (isInArray(JUNGLE_GRASS_REMOVE, item2.itemid) ) then
+		doRemoveItem(item2.uid)
+		return true
+	elseif (isInArray(JUNGLE_GRASS_TRANSFORM, item2.itemid) ) then
+		doTransformItem(item2.uid, item2.itemid - 1)
+		doDecayItem(item2.uid)
+		ret = true
+	elseif (isInArray(SPIDER_WEB, item2.itemid) ) then
+		doTransformItem(item2.uid, item2.itemid +6)
+		doDecayItem(item2.uid)
+		ret = true
+	end
+
+	if item2.actionid ~= 0 and ret then
+		doSetItemActionId(item2.uid, item2.actionid)
+	end
+
+	return ret
+end
+
+function useScythe(cid, item, frompos, item2, topos)
+
+local ITEM_PRE_WHEAT = 2739
+local ITEM_WHEAT = 2737
+local ITEM_BUNCH_WHEAT = 2694
+local ITEM_PRE_SUGAR_CANE = 5471
+local ITEM_SUGAR_CANE = 5463
+local ITEM_BUNCH_SUGAR_CANE = 5467
+
+if (item2.itemid == ITEM_PRE_WHEAT) then
+		doTransformItem(item2.uid, ITEM_WHEAT)
+		doCreateItem(ITEM_BUNCH_WHEAT, 1, topos)
+	elseif (item2.itemid == ITEM_PRE_SUGAR_CANE) then
+		doTransformItem(item2.uid, ITEM_SUGAR_CANE)
+		doCreateItem(ITEM_BUNCH_SUGAR_CANE, 1, topos)
+	else 
+		return false
+	end
+	doDecayItem(item2.uid)
+	return true
+end
+
