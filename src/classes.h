5c4b62e16dd02f996b5c019dea1b555c1f99dd97
Konrad Kuśnierz <iryont@gmail.com>
2013-09-30 18:58:30 +0200

    Create classes.h

---
+++ src/classes.h
@@ -0,0 +1,81 @@
+//////////////////////////////////////////////////////////////////////
+// OpenTibia - an opensource roleplaying game
+//////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//////////////////////////////////////////////////////////////////////
+
+#ifndef __OTSERV_CLASSES_H__
+#define __OTSERV_CLASSES_H__
+
+class Cylinder;
+class Thing;
+class Game;
+class Map;
+class IOMap;
+class Tile;
+class HouseTile;
+class Player;
+class Actor;
+class Creature;
+class ItemType;
+class Item;
+class Container;
+class Depot;
+class Teleport;
+class TrashHolder;
+class Door;
+class MagicField;
+class BedItem;
+class Town;
+class House;
+class Spawn;
+class RSA;
+class Party;
+class ConditionEffect;
+class Condition;
+class Position;
+class PositionEx;
+class CombatSource;
+class PropStream;
+class PropWriteStream;
+struct Account;
+class Protocol;
+class ProtocolGame;
+class OutputMessage;
+class NetworkMessage;
+class SchedulerTask;
+class Connection;
+class ServiceBase;
+class ServicePort;
+struct LightInfo;
+class CreatureType;
+class InternalCreatureType;
+class ServiceManager;
+class Commands;
+class ChatChannel;
+struct SpellBlock;
+struct OutfitType;
+struct Outfit;
+class Waypoint;
+struct FindPathParams;
+class QTreeLeafNode;
+struct LootBlock;
+struct Quest;
+struct ShopItem;
+class Vocation;
+
+#endif
