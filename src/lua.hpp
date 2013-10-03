d25e790218b05a8713f88aec660f722b3d111edd
Konrad Kuśnierz <iryont@gmail.com>
2013-10-03 18:54:10 +0200

    Update lua.hpp

--- src/lua.hpp
+++ src/lua.hpp
@@ -23,9 +23,9 @@
 
 extern "C"
 {
-        #include <lua.h>
-        #include <lauxlib.h>
-        #include <lualib.h>
+        #include "lua.h"
+        #include "lauxlib.h"
+        #include "lualib.h"
 }
 
 #endif
