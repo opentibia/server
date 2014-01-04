51542154d097c14b9b06f0c445680c69a71b2a2b
Konrad Kuśnierz <iryont@gmail.com>
2014-01-04 18:18:03 +0100

    Fix for new database structure

--- src/ban.cpp
+++ src/ban.cpp
@@ -86,7 +86,7 @@ bool BanManager::isIpDisabled(uint32_t clientip)
 	if(it != ipLoginMap.end()){
 		uint32_t loginTimeout = (uint32_t)g_config.getNumber(ConfigManager::LOGIN_TIMEOUT) / 1000;
 		if( (it->second.numberOfLogins >= (uint32_t)g_config.getNumber(ConfigManager::LOGIN_TRIES)) &&
-			((uint32_t)currentTime < (uint32_t)it->second.lastLoginTime + loginTimeout) )
+				((uint32_t)currentTime < (uint32_t)it->second.lastLoginTime + loginTimeout) )
 		{
 			banLock.unlock();
 			return true;
@@ -99,24 +99,22 @@ bool BanManager::isIpDisabled(uint32_t clientip)
 
 bool BanManager::isIpBanished(uint32_t clientip, uint32_t mask /*= 0xFFFFFFFF*/) const
 {
-	if(clientip == 0) return false;
+	if(clientip == 0){
+		return false;
+	}
+	
 	DatabaseDriver* db = DatabaseDriver::instance();
 
 	DBQuery query;
-	query <<
-		"SELECT "
-			"COUNT(*) AS `count` "
-		"FROM "
-			"`bans` "
-		"WHERE "
-			"`type` = " << BAN_IPADDRESS << " AND "
-			"((" << clientip << " & " << mask << " & `param`) = (`value` & `param` & " << mask << ")) AND "
-			"`active` = 1 AND "
-			"(`expires` >= " << std::time(NULL) << " OR `expires` <= 0)";
+	query << "SELECT COUNT(*) AS `count` "
+			 "FROM `ip_bans` "
+			 "INNER JOIN `bans` ON `bans`.`id` = `ip_bans`.`ban_id` "
+			 "WHERE " << "((" << clientip << " & " << mask << " & `mask`) = (`ip` & `mask` & " << mask << ")) AND `active` = 1 AND (`expires` >= unix_timestamp(now()) OR `expires` <= 0)"
 
 	DBResult_ptr result;
-	if(!(result = db->storeQuery(query.str())))
+	if(!(result = db->storeQuery(query.str()))){
 		return false;
+	}
 
 	int t = result->getDataInt("count");
 	return t > 0;
@@ -126,21 +124,19 @@ bool BanManager::isPlayerBanished(uint32_t playerId) const
 {
 	DatabaseDriver* db = DatabaseDriver::instance();
 
-	DBQuery query;
-	query <<
-		"SELECT "
-			"COUNT(*) AS `count` "
-		"FROM "
-			"`bans` "
-		"WHERE "
-			"`type` = " << BAN_PLAYER << " AND "
-			"`value` = " << playerId << " AND "
-			"`active` = 1 AND "
-			"(`expires` >= " << std::time(NULL) << " OR `expires` <= 0)";
+	DBQuery query;	
+	query << "SELECT COUNT(*) AS `count` "
+			 "FROM `player_bans` "
+			 "INNER JOIN `bans` ON `bans`.`id` = `player_bans`.`ban_id` "
+			 "WHERE "
+			 "`player_id` = " << playerId << " AND "
+			 "`active` = 1 AND "
+			 "(`expires` >= unix_timestamp(now()) OR `expires` <= 0)";
 
 	DBResult_ptr result;
-	if(!(result = db->storeQuery(query.str())))
+	if(!(result = db->storeQuery(query.str()))){
 		return false;
+	}
 
 	int t = result->getDataInt("count");
 	return t > 0;
@@ -150,8 +146,7 @@ bool BanManager::isPlayerBanished(const std::string& name) const
 {
 	uint32_t playerId;
 	std::string n = name;
-	return IOPlayer::instance()->getGuidByName(playerId, n)
-		&& isPlayerBanished(playerId);
+	return IOPlayer::instance()->getGuidByName(playerId, n) && isPlayerBanished(playerId);
 }
 
 bool BanManager::isAccountBanished(uint32_t accountId) const
@@ -159,20 +154,17 @@ bool BanManager::isAccountBanished(uint32_t accountId) const
 	DatabaseDriver* db = DatabaseDriver::instance();
 
 	DBQuery query;
-	query <<
-		"SELECT "
-			"COUNT(*) AS `count` "
-		"FROM "
-			"`bans` "
-		"WHERE "
-			"`type` = " << BAN_ACCOUNT << " AND "
-			"`value` = " << accountId << " AND "
-			"`active` = 1 AND "
-			"(`expires` >= " << std::time(NULL) << " OR `expires` <= 0)";
+	query << "SELECT (COUNT*) as `count` ";
+			 "FROM `account_bans` "
+			 "INNER JOIN `bans` ON `bans`.`id` = `account_bans`.`ban_id` "
+			 "WHERE "
+			 "`account_id` = " << accountId << " AND "
+			 "`active` = 1 AND "
+			 "(`expires` >= unix_timestamp(now()) OR `expires` <= 0)";
 
 	DBResult_ptr result;
 	if(!(result = db->storeQuery(query.str())))
-		return false;
+	return false;
 
 	int t = result->getDataInt("count");
 	return t > 0;
@@ -180,7 +172,10 @@ bool BanManager::isAccountBanished(uint32_t accountId) const
 
 void BanManager::addLoginAttempt(uint32_t clientip, bool isSuccess)
 {
-	if(clientip == 0) return;
+	if(clientip == 0){
+		return;
+	}
+	
 	banLock.lock();
 
 	time_t currentTime = std::time(NULL);
@@ -195,216 +190,331 @@ void BanManager::addLoginAttempt(uint32_t clientip, bool isSuccess)
 	}
 
 	if(it->second.numberOfLogins >= (uint32_t)g_config.getNumber(ConfigManager::LOGIN_TRIES))
-		it->second.numberOfLogins = 0;
+	it->second.numberOfLogins = 0;
 
 	uint32_t retryTimeout = (uint32_t)g_config.getNumber(ConfigManager::RETRY_TIMEOUT) / 1000;
-	if(!isSuccess || ((uint32_t)currentTime < (uint32_t)it->second.lastLoginTime + retryTimeout))
+	if(!isSuccess || ((uint32_t)currentTime < (uint32_t)it->second.lastLoginTime + retryTimeout)){
 		++it->second.numberOfLogins;
-	else
+	} else {
 		it->second.numberOfLogins = 0;
+	}
 
 	it->second.lastLoginTime = currentTime;
 	banLock.unlock();
 }
 
 bool BanManager::addIpBan(uint32_t ip, uint32_t mask, int32_t time,
-	uint32_t adminid, std::string comment) const
+uint32_t adminid, std::string comment) const
 {
-	if(ip == 0 || mask == 0) return false;
-	DatabaseDriver* db = DatabaseDriver::instance();
-
-	DBInsert stmt(db);
-	stmt.setQuery("INSERT INTO `bans` (`type`, `value`, `param`, `expires`, `added`, `admin_id`, `comment`) VALUES ");
-
-	DBQuery query;
-	query << BAN_IPADDRESS << ", " << ip << ", " << mask << ", " << time << ", ";
-	query << std::time(NULL) << ", " << adminid << ", " << db->escapeString(comment);
-
-
-	if(!stmt.addRow(query.str()))
+	if(ip == 0 || mask == 0){
 		return false;
-	return stmt.execute();
+	}
+	
+	DatabaseDriver* db = DatabaseDriver::instance();
+	
+	DBInsert stmt(db);
+	DBQuery query;
+	
+	stmtBan.setQuery("INSERT INTO `bans` (`expires`, `added`, `active`, `admin_id`, `comment`) VALUES ");
+	query << time << ", " << std::time(NULL) << ", 1, " << adminid << ", " << db->escapeString(comment);
+	
+	if(!stmt.addRow(query.str())){
+		return false;
+	}
+	
+	if(!stmt.execute()){
+		return false;
+	}
+	
+	int banId = db->getLastInsertedRowID();
+	
+	stmt.setQuery("INSERT INTO `ip_bans` (`ban_id`, `ip`, `mask`) VALUES ");
+	query.reset();
+	query << banId << ", " << ip << ", " << mask;
+	
+	if(!stmt.addRow(query.str())){
+		return false;
+	}
+	
+	if(!stmt.execute()){
+		return false;
+	}
+	
+	return true;
 }
 
 bool BanManager::addPlayerBan(uint32_t playerId, int32_t time, uint32_t adminid,
-	std::string comment, std::string statement, uint32_t reason, ViolationAction action) const
+std::string comment, std::string statement, uint32_t reason, ViolationAction action) const
 {
-	if(playerId == 0) return false;
+	if(playerId == 0){
+		return false;
+	}
+	
 	DatabaseDriver* db = DatabaseDriver::instance();
 
 	DBInsert stmt(db);
-	stmt.setQuery("INSERT INTO `bans` (`type`, `value`, `expires`, `added`, `admin_id`, `comment`, `statement`, `reason`, `action`) VALUES ");
-
 	DBQuery query;
-	query << BAN_PLAYER << ", " << playerId << ", " << time << ", " << std::time(NULL) << ", " << adminid << ", ";
-	query << db->escapeString(comment) << ", " << db->escapeString(statement) << ", " << reason << ", " << action;
-
-	if(!stmt.addRow(query.str()))
+	
+	stmtBan.setQuery("INSERT INTO `bans` (`expires`, `added`, `active`, `admin_id`, `comment`) VALUES ");
+	query << time << ", " << std::time(NULL) << ", 1, " << adminid << ", " << db->escapeString(comment);
+	
+	if(!stmt.addRow(query.str())){
 		return false;
-	return stmt.execute();
+	}
+	
+	if(!stmt.execute()){
+		return false;
+	}
+	
+	int banId = db->getLastInsertedRowID();
+	
+	stmt.setQuery("INSERT INTO `player_bans` (`ban_id`, `player_id`) VALUES ");
+	query.reset();
+	query << banId << ", " << playerId;
+	
+	if(!stmt.addRow(query.str())){
+		return false;
+	}
+	
+	if(!stmt.execute()){
+		return false;
+	}
+	
+	return true;
 }
 
 bool BanManager::addPlayerBan(const std::string& name, int32_t time, uint32_t adminid,
-	std::string comment, std::string statement, uint32_t reason, ViolationAction action) const
+std::string comment, std::string statement, uint32_t reason, ViolationAction action) const
 {
 	uint32_t guid = 0;
 	std::string n = name;
-	return IOPlayer::instance()->getGuidByName(guid, n) &&
-		addPlayerBan(guid, time, adminid, comment, statement, reason, action);
+	return IOPlayer::instance()->getGuidByName(guid, n) && addPlayerBan(guid, time, adminid, comment, statement, reason, action);
 }
 
 bool BanManager::addPlayerStatement(uint32_t playerId, uint32_t adminid, std::string comment,
-	std::string statement, uint32_t reason, ViolationAction action) const
+std::string statement, uint32_t reason, ViolationAction action) const
 {
-	if(playerId == 0) return false;
-	DatabaseDriver* db = DatabaseDriver::instance();
-
-	DBInsert stmt(db);
-	stmt.setQuery("INSERT INTO `bans` (`type`, `value`, `expires`, `added`, `admin_id`, `comment`, `statement`, `reason`, `action`) VALUES ");
-
-	DBQuery query;
-	query << BAN_STATEMENT << ", " << playerId << ", " << -1 << ", " << std::time(NULL) << ", " << adminid << ", ";
-	query << db->escapeString(comment) << ", " << db->escapeString(statement) << ", " << reason << ", " << action;
-
-	if(!stmt.addRow(query.str()))
+	if(playerId == 0){
 		return false;
-	return stmt.execute();
+	}
+	
+	/** 
+		There is no place for statements in database 
+	*/
+	
+	return false;
 }
 
 bool BanManager::addAccountBan(uint32_t account, int32_t time, uint32_t adminid,
-	std::string comment, std::string statement, uint32_t reason, ViolationAction action) const
+std::string comment, std::string statement, uint32_t reason, ViolationAction action) const
 {
-	if(account == 0) return false;
+	if(account == 0){
+		return false;
+	}
+	
 	DatabaseDriver* db = DatabaseDriver::instance();
 
 	DBInsert stmt(db);
 	DBQuery query;
-	stmt.setQuery("INSERT INTO `bans` (`type`, `value`, `expires`, `added`, `admin_id`, `comment`, `statement`, `reason`, `action`) VALUES ");
-
-	query << BAN_ACCOUNT << ", " << account << ", " << time << ", " << std::time(NULL) << ", " << adminid << ", ";
-	query << db->escapeString(comment) << ", " << db->escapeString(statement) << ", " << reason << ", " << action;
-
-	if(!stmt.addRow(query.str())) return false;
-	return stmt.execute();
+	
+	stmtBan.setQuery("INSERT INTO `bans` (`expires`, `added`, `active`, `admin_id`, `comment`) VALUES ");
+	query << time << ", " << std::time(NULL) << ", 1, " << adminid << ", " << db->escapeString(comment);
+	
+	if(!stmt.addRow(query.str())){
+		return false;
+	}
+	
+	if(!stmt.execute()){
+		return false;
+	}
+	
+	int banId = db->getLastInsertedRowID();
+	
+	stmt.setQuery("INSERT INTO `account_bans` (`ban_id`, `account_id`) VALUES ");
+	query.reset();
+	query << banId << ", " << account;
+	
+	if(!stmt.addRow(query.str())){
+		return false;
+	}
+	
+	if(!stmt.execute()){
+		return false;
+	}
+	
+	return true;
 }
 
 bool BanManager::addAccountNotation(uint32_t account, uint32_t adminid, std::string comment,
-	std::string statement, uint32_t reason, ViolationAction action) const
+std::string statement, uint32_t reason, ViolationAction action) const
 {
-	if(account == 0) return false;
-	DatabaseDriver* db = DatabaseDriver::instance();
-
-	DBInsert stmt(db);
-	DBQuery query;
-	stmt.setQuery("INSERT INTO `bans` (`type`, `value`, `expires`, `added`, `admin_id`, `comment`, `statement`, `reason`, `action`) VALUES ");
-
-	query << BAN_NOTATION << ", " << account << ",  " << -1 << ", " << std::time(NULL) << ", " << adminid << ", ";
-	query << db->escapeString(comment) << ", " << db->escapeString(statement) << ", " << reason << ", " << action;
-
-	if(!stmt.addRow(query.str())) return false;
-	return stmt.execute();
+	if(account == 0){
+		return false;
+	}
+	
+	/** 
+		There is no place for notations in database 
+	*/
+	
+	return false;
 }
 
 bool BanManager::removeIpBans(uint32_t ip, uint32_t mask) const
 {
-	if(!isIpBanished(ip, mask)) return false;
+	if(!isIpBanished(ip, mask)){
+		return false;
+	}
+	
 	DatabaseDriver* db = DatabaseDriver::instance();
 
 	DBQuery query;
-	query << "UPDATE `bans` SET `active` = 0 WHERE `type` = " << BAN_IPADDRESS << " AND (`value` & `param` & " << mask << ") = (" << ip << " & `param` & " << mask << ")" << " AND `active` = 1";
-	return db->executeQuery(query.str());
+	query << "SELECT `ban_id` AS `id` "
+			 "FROM `ip_bans` "
+			 "INNER JOIN `bans` ON `bans`.`id` = `ip_bans`.`ban_id` "
+			 "WHERE " << "((" << ip << " & " << mask << " & `mask`) = (`ip` & `mask` & " << mask << ")) AND `active` = 1"
+	
+	for(DBResult_ptr result = db->storeQuery(query.str()); result; result = result->advance()){
+		query.reset();
+		query << "UPDATE `bans` SET `active` = 0 WHERE `id` = " << result->getDataInt("id");
+		if(!db->executeQuery(query.str())){
+			return false;
+		}
+	}
+
+	return true;
 }
 
 bool BanManager::removePlayerBans(uint32_t guid) const
 {
-	if(!isPlayerBanished(guid)) return false;
+	if(!isPlayerBanished(guid)){
+		return false;
+	}
+	
 	DatabaseDriver* db = DatabaseDriver::instance();
 
 	DBQuery query;
-	query << "UPDATE `bans` SET `active` = 0 WHERE `type` = " << BAN_PLAYER << " AND `value` = " << guid << " AND `active` = 1";
-	return db->executeQuery(query.str());
+	query << "SELECT `ban_id` AS `id` "
+			 "FROM `player_bans` "
+			 "INNER JOIN `bans` ON `bans`.`id` = `player_bans`.`ban_id` "
+			 "WHERE `player_id` = " << guid << " AND `active` = 1"
+	
+	for(DBResult_ptr result = db->storeQuery(query.str()); result; result = result->advance()){
+		query.reset();
+		query << "UPDATE `bans` SET `active` = 0 WHERE `id` = " << result->getDataInt("id");
+		if(!db->executeQuery(query.str())){
+			return false;
+		}
+	}
+
+	return true;
 }
 
 bool BanManager::removePlayerBans(const std::string& name) const
 {
 	uint32_t playerId = 0;
 	std::string n = name;
-	return IOPlayer::instance()->getGuidByName(playerId, n)
-		&& removePlayerBans(playerId);
+	return IOPlayer::instance()->getGuidByName(playerId, n) && removePlayerBans(playerId);
 }
 
 bool BanManager::removeAccountBans(uint32_t accno) const
 {
-	if(!isAccountBanished(accno)) return false;
+	if(!isAccountBanished(accno)){
+		return false;
+	}
+	
 	DatabaseDriver* db = DatabaseDriver::instance();
 
 	DBQuery query;
-	query << "UPDATE `bans` SET `active` = 0 WHERE `type` = " << BAN_ACCOUNT << " AND `value` = " << accno << " AND `active` = 1";
-	return db->executeQuery(query.str());
+	query << "SELECT `ban_id` AS `id` "
+			 "FROM `account_bans` "
+			 "INNER JOIN `bans` ON `bans`.`id` = `account_bans`.`ban_id` "
+			 "WHERE `account_id` = " << guid << " AND `active` = 1"
+	
+	for(DBResult_ptr result = db->storeQuery(query.str()); result; result = result->advance()){
+		query.reset();
+		query << "UPDATE `bans` SET `active` = 0 WHERE `id` = " << result->getDataInt("id");
+		if(!db->executeQuery(query.str())){
+			return false;
+		}
+	}
+
+	return true;
 }
 
 bool BanManager::removeNotations(uint32_t accno) const
 {
-	DatabaseDriver* db = DatabaseDriver::instance();
-	DBQuery query;
-
-	query << "UPDATE `bans` SET `active` = 0 WHERE `type` = " << BAN_NOTATION << " AND `value` = " << accno << " AND `active` = 1";
-	return db->executeQuery(query.str());
+	/** 
+		There is no place for notations in database 
+	*/
+	
+	return false;
 }
 
 uint32_t BanManager::getNotationsCount(uint32_t account) const
 {
-	DatabaseDriver* db = DatabaseDriver::instance();
-	DBResult_ptr result;
-
-	DBQuery query;
-	query << "SELECT COUNT(`id`) AS `count` FROM `bans` WHERE `value` = " << account << " AND `type` = " << BAN_NOTATION << " AND `active` = 1";
-	if(!(result = db->storeQuery(query.str())))
-		return 0;
-
-	const uint32_t count = result->getDataInt("count");
-	return count;
+	/** 
+		There is no place for notations in database 
+	*/
+	
+	return 0;
 }
 
 std::vector<Ban> BanManager::getBans(BanType_t type)
 {
-	assert(type == BAN_IPADDRESS || type == BAN_PLAYER || type == BAN_ACCOUNT || type == BAN_NOTATION);
+	std::vector<Ban> result;
 	DatabaseDriver* db = DatabaseDriver::instance();
-
 	DBQuery query;
-	query <<
-		"SELECT "
-			"`id`, "
-			"`value`, "
-			"`param`, "
-			"`expires`, "
-			"`added`, "
-			"`admin_id`, "
-			"`comment`, "
-			"`reason` "
-		"FROM "
-			"`bans` "
-		"WHERE "
-			"`type` = " << type << " AND "
-			"`active` = 1 AND " <<
-			"(`expires` >= " << std::time(NULL) << " OR `expires` = 0)";
-
-	std::vector<Ban> vec;
-	for (DBResult_ptr result = db->storeQuery(query.str()); result; result = result->advance()) {
-		Ban ban;
-		ban.type = type;
-		ban.id = result->getDataInt("id");
-		ban.value = result->getDataString("value");
-		ban.param = result->getDataString("param");
-		ban.expires = (time_t)result->getDataLong("expires");
-		ban.added = (uint32_t)result->getDataLong("id");
-		ban.adminId = result->getDataInt("admin_id");
-		ban.reason = result->getDataInt("reason");
-		ban.action = ViolationAction(result->getDataInt("action"));
-		ban.comment = result->getDataString("comment");
-		ban.statement = result->getDataString("statement");
-
-		vec.push_back(ban);
+	
+	switch(type){
+		case BAN_IPADDRESS: {
+			query << "SELECT `id`, `ip` as `value`, `mask` as `param`, `expires`, `added`, `admin_id`, `comment`"
+					 "FROM `ip_bans` "
+					 "INNER JOIN `bans` ON `bans`.`id` = `ip_bans`.`ban_id` "
+					 "WHERE "
+					 "`active` = 1 AND "
+					 "(`expires` >= unix_timestamp(now()) OR `expires` <= 0)";
+			break;
+		}
+		
+		case BAN_PLAYER: {
+			query << "SELECT `id`, `player_id` as `value`, `expires`, `added`, `admin_id`, `comment`"
+					 "FROM `player_bans` "
+					 "INNER JOIN `bans` ON `bans`.`id` = `player_bans`.`ban_id` "
+					 "WHERE "
+					 "`active` = 1 AND "
+					 "(`expires` >= unix_timestamp(now()) OR `expires` <= 0)";
+			break;
+		}
+		
+		case BAN_ACCOUNT: {
+			query << "SELECT `id`, `account_id` as `value`, `expires`, `added`, `admin_id`, `comment`"
+					 "FROM `account_bans` "
+					 "INNER JOIN `bans` ON `bans`.`id` = `account_bans`.`ban_id` "
+					 "WHERE "
+					 "`active` = 1 AND "
+					 "(`expires` >= unix_timestamp(now()) OR `expires` <= 0)";
+			break;
+		}
+		
+		default: break;
 	}
-	return vec;
+	
+	if(query.good()){
+		for (DBResult_ptr result = db->storeQuery(query.str()); result; result = result->advance()) {
+			Ban ban;
+			
+			ban.type = type;
+			ban.id = result->getDataInt("id");
+			ban.value = result->getDataString("value");
+			ban.param = result->getDataString("param");
+			ban.expires = (time_t)result->getDataLong("expires");
+			ban.added = (uint32_t)result->getDataLong("added");
+			ban.adminId = result->getDataInt("admin_id");
+			ban.comment = result->getDataString("comment");
+			
+			result.push_back(ban);
+		}
+	}
+	
+	return result;
 }
